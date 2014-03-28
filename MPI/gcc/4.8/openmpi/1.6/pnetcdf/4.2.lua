c1296ec4916ed81ea7db5f12e250c4f2624fe365
Jingchao Zhang <jingchao@login.crane.hcc.unl.edu>
2014-03-28 15:29:48 -0500

    Parallel NetCDF

---
+++ MPI/gcc/4.8/openmpi/1.6/pnetcdf/4.2.lua
@@ -0,0 +1,22 @@
+local help_message = [[
+netCDF 4.2
+
+This module loads the netCDF, built with the GCC 
+compilers and OpenMPI.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: netCDF")
+whatis("Version: 4.2")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.unidata.ucar.edu/downloads/netcdf/index.jsp")
+
+prepend_path("NETCDF",              "/util/opt/netcdf/4.2/openmpi/1.6/gcc/4.8")
+prepend_path("PATH",                "/util/opt/netcdf/4.2/openmpi/1.6/gcc/4.8/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/netcdf/4.2/openmpi/1.6/gcc/4.8/lib")
+prepend_path("INCLUDE",             "/util/opt/netcdf/4.2/openmpi/1.6/gcc/4.8/include")
+
+family("pnetcdf")
