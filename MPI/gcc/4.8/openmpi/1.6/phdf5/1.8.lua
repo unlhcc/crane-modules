92a82969cf995061e73045061f1005802c28f87b
Jingchao Zhang <jingchao@login.crane.hcc.unl.edu>
2014-03-28 13:50:55 -0500

    hdf5 parallel

---
+++ MPI/gcc/4.8/openmpi/1.6/phdf5/1.8.lua
@@ -0,0 +1,22 @@
+local help_message = [[
+Parallel hdf5 1.8
+
+This module loads the hdf5, built with the GCC,OpenMPI
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: hdf5")
+whatis("Version: 1.8")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.hdfgroup.org/HDF5/release/obtain5.html#obtain")
+
+prepend_path("PATH",                "/util/opt/hdf5/1.8/openmpi/gcc/4.8/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/hdf5/1.8/openmpi/gcc/4.8/lib")
+prepend_path("MANPATH",             "/util/opt/hdf5/1.8/openmpi/gcc/4.8/share/man")
+prepend_path("INCLUDE",             "/util/opt/hdf5/1.8/openmpi/gcc/4.8/include")
+
+family("phdf5")
