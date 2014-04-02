cca928f2ff69abfbf1da0dab155b3da4f45dfd62
acaprez2 <acaprez2@unl.edu>
2014-04-02 17:14:49 -0500

    Added modulefile for BWA (http://bio-bwa.sourceforge.net)

---
+++ Core/bwa/0.7.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads bwa built with GCC.
+This module makes available the bwa executable. Documentation for bwa is available online at the publisher\'s website: http://bio-bwa.sourceforge.net/bwa.shtml
+
+Version 0.7
+]])
+
+whatis("Name: bwa")
+whatis("Version: 0.7")
+whatis("Category: computational biology, genomics")
+whatis("Keywords:  Biology, Genomics, Alignment, Sequencing")
+whatis("Description: bwa - Burrows-Wheeler Alignment Tool")
+whatis("URL: http://bio-bwa.sourceforge.net/")
+
+prepend_path("PATH"       ,"/util/opt/bwa/0.7/gcc/4.4/bin")
