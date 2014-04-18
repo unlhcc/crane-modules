c4d48ba98c1ba1e471c387a4465630223bb46f19
Jun Wang <jwang@login.crane.hcc.unl.edu>
2014-04-18 10:22:03 -0500

    Add mpich2 environment to atk module

--- Core/atk/13.8.1.lua
+++ Core/atk/13.8.1.lua
@@ -7,9 +7,12 @@ help(help_message,"\n")
 whatis("Name: ATK")
 whatis("Keywords: Application, Chemistry")
 
---load("compiler/pgi/13","openmpi/1.6")
+load("compiler/gcc/4.7")
 
 setenv("QUANTUM_LICENSE_PATH", "6200@dmol4b.unl.edu")
 setenv("LMX_LICENSE_PATH", "6200@dmol4b.unl.edu")
 
-prepend_path("PATH","/util/opt/atk/13.8.1/vnl/bin:/util/opt/atk/13.8.1/atkpython/bin:/util/opt/atk/13.8.1/license")
+prepend_path("PATH","/util/opt/atk/13.8.1/vnl/bin:/util/opt/atk/13.8.1/atkpython/bin:/util/opt/atk/13.8.1/license:/util/opt/mpich2/1.5/gcc/4.7.1/bin")
+prepend_path("LD_LIBRARY_PATH","/util/opt/mpich2/1.5/gcc/4.7.1/lib")
+prepend_path("MANPATH","/util/opt/mpich2/1.5/gcc/4.7.1/share/man")
+prepend_path("INCLUDE","/util/opt/mpich2/1.5/gcc/4.7.1/include")
