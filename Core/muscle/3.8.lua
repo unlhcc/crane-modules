575dee202f0ff1012b7f4fb5c3ea1405962e3aa6
acaprez2 <acaprez2@unl.edu>
2014-04-25 14:13:34 -0500

    Added module for muscle.

---
+++ Core/muscle/3.8.lua
@@ -0,0 +1,19 @@
+help (
+[[
+The muscle module file defines the following environment variables:
+PATH for the location of the muscle distribution binary. 
+Documentation can be found online at http://www.drive5.com/muscle/
+
+Version 3.8
+
+]])
+
+whatis("Name: MUSCLE")
+whatis("Version: 3.8")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Alignment, Sequencing")
+whatis("URL: http://www.drive5.com/muscle/")
+whatis("Description: Popular multiple alignment software")
+
+prepend_path("PATH"       ,"/util/opt/muscle/3.8/gcc/4.4/bin")
+
