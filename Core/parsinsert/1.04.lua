ee343cde0efa71ee7ef811945f0625a116cb33b2
acaprez2 <acaprez2@unl.edu>
2014-04-25 14:56:15 -0500

    Added module for ParsInsert.

---
+++ Core/parsinsert/1.04.lua
@@ -0,0 +1,17 @@
+help(
+[[
+The parsinsert module file defines the following environment variables:
+PATH for the location of the parsinsert distribution executables.
+
+Version 1.04
+]]
+)
+
+whatis("Name: ParsInsert")
+whatis("Version: 1.04")
+whatis("Category: computational biology, genomics, taxonomy")
+whatis("Keywords: Biology, Genomics, Sequencing")
+whatis("URL: http://parsinsert.sourceforge.net")
+whatis("Description: ParsInsert efficiently produces both a phylogenetic tree and taxonomic classification for sequences for microbial community sequence analysis. This is a C++ implementation of the Parsimonious Insertion algorithm")
+
+prepend_path("PATH",              "/util/opt/parsinsert/1.04/gcc/4.4/bin")
