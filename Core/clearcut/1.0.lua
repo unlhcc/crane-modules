680b178ffe1681a7139167f12d689775d43e4fe8
acaprez2 <acaprez2@unl.edu>
2014-04-25 13:28:22 -0500

    Added module for clearcut.

---
+++ Core/clearcut/1.0.lua
@@ -0,0 +1,16 @@
+help (
+[[
+The clearcut module file defines the following environment variables:
+PATH for the location of the clearcut distribution executables.
+Version 1.0
+]])
+
+whatis("Name: clearcut")
+whatis("Version: 1.0")
+whatis("Category: computational biology, phylogenetics")
+whatis("Keywords: Biology, Genomics, Bioinformatics")
+whatis("Description: Clearcut - The reference implementation for Relaxed Neighbor Joining (RNJ)")
+whatis("URL: http://bioinformatics.hungry.com/clearcut")
+
+prepend_path("PATH"		,"/util/opt/clearcut/1.0/gcc/4.4/bin")
+
