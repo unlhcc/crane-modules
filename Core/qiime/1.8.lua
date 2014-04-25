eb8c4abe17a0fb7e5a471d68a46d0818818a4af2
acaprez2 <acaprez2@unl.edu>
2014-04-25 17:41:44 -0500

    Added module for qiime 1.8.

---
+++ Core/qiime/1.8.lua
@@ -0,0 +1,47 @@
+help(
+[[
+The qiime module file defines the following environment variables:
+
+Version 1.8
+]]
+)
+
+whatis("Name: QIIME: Quantitative Insights Into Microbial Ecology")
+whatis("Version: 1.8")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Assembly, QIIME, sequencing")
+whatis("URL: http://qiime.org")
+whatis("Description: QIIME is an open source software package for comparison and analysis of microbial communities, primarily based on high-throughput amplicon sequencing data (such as SSU rRNA) generated on a variety of platforms, but also supporting analysis of other types of data (such as shotgun metagenomic data).")
+
+prepend_path("QIIME_HOME",    "/util/opt/qiime/1.8/gcc/4.4")
+prepend_path("QIIME_CONFIG_FP", "/util/opt/qiime/1.8/gcc/4.4/qiime_config")
+prepend_path("PATH",		"/util/opt/qiime/1.8/gcc/4.4/bin")
+prepend_path("PATH",            "/util/opt/qiime/1.8/gcc/4.4/deps/blast-2.2.22/bin")
+prepend_path("PATH",            "/util/opt/qiime/1.8/gcc/4.4/deps/cd-hit-3.1.1")
+prepend_path("PATH",            "/util/opt/qiime/1.8/gcc/4.4/deps/cdbfasta-0.99")
+prepend_path("PATH",		"/util/opt/qiime/1.8/gcc/4.4/deps/microbiomeutil_2010-04-29/ChimeraSlayer")
+prepend_path("PATH",		"/util/opt/qiime/1.8/gcc/4.4/deps/mothur-1.25.0/bin")
+prepend_path("PATH",		"/util/opt/qiime/1.8/gcc/4.4/deps/raxml-7.3.0/bin")
+prepend_path("PATH",		"/util/opt/qiime/1.8/gcc/4.4/deps/infernal-1.0.2/bin")
+prepend_path("PATH",		"/util/opt/qiime/1.8/gcc/4.4/deps/rtax-0.984")
+prepend_path("PATH",            "/util/opt/qiime/1.8/gcc/4.4/deps/rtax-0.984/scripts")
+prepend_path("PATH",		"/util/opt/qiime/1.8/gcc/4.4/deps/cytoscape-v2.7.0")
+prepend_path("PATH",		"/util/opt/qiime/1.8/gcc/4.4/deps/AmpliconNoiseV1.27/bin")
+prepend_path("PATH",		"/util/opt/qiime/1.8/gcc/4.4/deps/AmpliconNoiseV1.27/Scripts")
+prepend_path("PATH",		"/util/opt/qiime/1.8/gcc/4.4/deps/blat-34/bin")
+
+-- Lmod only allows one version of a module to be loaded at a time, so
+-- prepend the usearch paths directly instead of loading modules.
+-- The binaries are named differently, so there's no collision.
+prepend_path("PATH",		"/util/opt/usearch/5.2/bin")
+prepend_path("PATH",            "/util/opt/usearch/6.1/bin")
+
+setenv("RDP_JAR_PATH",		"/util/opt/qiime/1.8/gcc/4.4/deps/rdp_classifier_2.2/rdp_classifier-2.2.jar")
+prepend_path("PYTHONPATH",	"/util/opt/qiime/1.8/gcc/4.4/lib/python2.7/site-packages")
+prepend_path("PYTHONPATH",	"/util/opt/qiime/1.8/gcc/4.4/deps/gdata-2.0.17/lib/python2.7/site-packages")
+setenv("BLASTMAT", 		"/util/opt/qiime/1.8/gcc/4.4/deps/blast-2.2.22/data")
+setenv("PYRO_LOOKUP_FILE",	"/util/opt/qiime/1.8/gcc/4.4/deps/AmpliconNoiseV1.27/Data/LookUp_E123.dat")
+setenv("SEQ_LOOKUP_FILE",	"/util/opt/qiime/1.8/gcc/4.4/deps/AmpliconNoiseV1.27/Data/Tran.dat")
+
+
+load("compiler/gcc/4.8","openmpi/1.6","python/2.7","R/3.0","bwa/0.7","tax2tree/1.0","clearcut/1.0","muscle/3.8","pplacer/1.1","parsinsert/1.04","mafft/7.149")
