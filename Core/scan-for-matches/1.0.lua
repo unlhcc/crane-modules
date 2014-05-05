15d4938827c7ea06fc1273488072fd64aaf3d769
acaprez2 <acaprez2@unl.edu>
2014-05-05 17:00:08 -0500

    Added module for Scan for Matches.

---
+++ Core/scan-for-matches/1.0.lua
@@ -0,0 +1,17 @@
+help(
+[[
+The scan-for-matches module file defines the following environment variables:
+PATH for the location of the distribution executables.
+
+Version 1.0
+]]
+)
+
+whatis("Name: scan-for-matches")
+whatis("Version: 1.0")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Sequencing")
+whatis("URL: http://blog.theseed.org/servers/2010/07/scan-for-matches.html")
+whatis("Description: scan_for_matches is a utility written in C for locating patterns in DNA or protein FASTA files.")
+
+prepend_path("PATH",              "/util/opt/scan_for_matches/1.0/gcc/4.4/bin")
