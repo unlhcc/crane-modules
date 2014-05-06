02783094e788b7bd770aaa1609a23c059a0366df
acaprez2 <acaprez2@unl.edu>
2014-05-06 15:39:06 -0500

    Added module for Prodigal 2.60.

---
+++ Core/prodigal/2.60.lua
@@ -0,0 +1,18 @@
+help(
+[[
+The prodigal module file defines the following environment variables:
+PATH for the location of the prodigal executables.
+
+Version 2.60
+]]
+)
+
+whatis("Name: Prodigal")
+whatis("Version: 2.60")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics")
+whatis("URL: http://prodigal.ornl.gov")
+whatis("Description: Prodigal (Prokaryotic Dynamic Programming Genefinding Algorithm) is a microbial (bacterial and archaeal) gene finding program developed at Oak Ridge National Laboratory and the University of Tennessee.")
+
+
+prepend_path("PATH",              "/util/opt/prodigal/2.60/gcc/4.4/bin")
