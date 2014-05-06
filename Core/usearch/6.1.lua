5a42b787003b71d579b8ebfdc97f738eb583cce7
acaprez2 <acaprez2@unl.edu>
2014-05-06 17:16:41 -0500

    Removed LmodMessage.
    Changed symlink name from `usearch61` to `usearch`, so no need
    for message anymore.

--- Core/usearch/6.1.lua
+++ Core/usearch/6.1.lua
@@ -14,8 +14,4 @@ whatis("Keywords: Biology, Genomics, Sequencing")
 whatis("URL: http://www.drive5.com/usearch")
 whatis("Description: USEARCH is a unique sequence analysis tool with thousands of users world-wide. USEARCH offers search and clustering algorithms that are often orders of magnitude faster than BLAST.")
 
-if (mode() == "load") then
-LmodMessage("The usearch program is named 'usearch61'.")
-end
-
 prepend_path("PATH",              "/util/opt/usearch/6.1/bin")
