750274405e78eb45871900f3d4efd8a987823b20
acaprez2 <acaprez2@unl.edu>
2014-05-06 14:59:23 -0500

    Added module for IDBA 1.1.

---
+++ Core/idba/1.1.lua
@@ -0,0 +1,18 @@
+help(
+[[
+The idba module file defines the following environment variables:
+PATH for the location of the idba distribution executables.
+
+Version 1.1
+]]
+)
+
+whatis("Name: IDBA")
+whatis("Version: 1.1")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Sequencing")
+whatis("URL: http://i.cs.hku.hk/~alse/hkubrg/projects/idba")
+whatis("Description: IDBA is a practical iterative De Bruijn Graph De Novo Assembler for sequence assembly in bioinfomatics.")
+
+
+prepend_path("PATH",              "/util/opt/idba/1.1/gcc/4.4/bin")
