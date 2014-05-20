075d9f2406723a17174282bd866039332042e829
acaprez2 <acaprez2@unl.edu>
2014-05-20 14:44:48 -0500

    Added module for CUDA 6.0.

---
+++ Core/cuda/6.0.lua
@@ -0,0 +1,26 @@
+help(
+[[
+This module loads the NVIDIA CUDA Toolkit and updates the $PATH 
+$LD_LIBRARY_PATH, $INCLUDE, and $MANPATH environment 
+variables to access the toolkit binaries, libraries, include 
+files, and available man pages, respectively.
+
+Version 6.0
+]]
+)
+
+whatis("Name: NVIDIA CUDA Toolkit")
+whatis("Version: 6.0")
+whatis("Category: compiler, runtime support")
+whatis("Keywords: compiler, cuda")
+whatis("URL: http://www.nvidia.com/object/cuda_home_new.html")
+whatis("Description: NVIDIA CUDA Toolkit for Linux.")
+
+
+prepend_path("PATH",              "/util/opt/cuda/6.0/bin")
+prepend_path("LD_LIBRARY_PATH",   "/util/opt/cuda/6.0/lib64")
+prepend_path("LD_LIBRARY_PATH",   "/util/opt/cuda/6.0/lib")
+prepend_path("MANPATH",		  "/util/opt/cuda/6.0/doc/man")
+setenv("CFLAGS",		  "-I/util/opt/cuda/6.0/include")
+setenv("CPPFLAGS",		  "-I/util/opt/cuda/6.0/include")
+setenv("FFLAGS",                  "-I/util/opt/cuda/6.0/include")
