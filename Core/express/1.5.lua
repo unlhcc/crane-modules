0379e49285c14b8d97ab3aeed198e0c65d7cd0a8
acaprez2 <acaprez2@unl.edu>
2014-06-12 12:01:02 -0500

    Added module for express 1.5.

---
+++ Core/express/1.5.lua
@@ -0,0 +1,17 @@
+help (
+[[
+This module loads express.
+This module makes available the express executable. Documentation for express is available online at the publisher\'s website: http://bio.math.berkeley.edu/eXpress/
+
+Version 1.5
+]])
+
+whatis("Name: express")
+whatis("Version: 1.5")
+whatis("Category: life sciences, genomics")
+whatis("Keywords: LSC, 2012Q2, Biology, Genomics")
+whatis("Description: express - Streaming quantification for high-throughput sequencing")
+whatis("URL: http://bio.math.berkeley.edu/eXpress/")
+
+prepend_path("PATH"       ,"/util/opt/express/1.5/bin")
+
