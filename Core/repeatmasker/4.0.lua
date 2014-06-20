7dbb38748c67dfaed7a60939b007b53241d9ebce
acaprez2 <acaprez2@unl.edu>
2014-06-20 17:11:20 -0500

    Added module for RepeatMasker 4.0.

---
+++ Core/repeatmasker/4.0.lua
@@ -0,0 +1,21 @@
+help(
+[[
+The repeatmasker module defines the following environment variables:
+PATH for the location of the repeatmasker distribution executables.
+
+Version 4.0
+]]
+)
+
+whatis("Name: RepeatMasker")
+whatis("Version: 4.0")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Alignment")
+whatis("URL: http://www.repeatmasker.org")
+whatis("Description: RepeatMasker is a program that screens DNA sequences for interspersed repeats and low complexity DNA sequences.")
+
+
+prepend_path("PATH",              "/util/opt/repeatmasker/4.0/gcc/4.4/RepeatMasker")
+prepend_path("PATH",		  "/util/opt/repeatmasker/4.0/gcc/4.4/bin")
+prepend_path("PATH",              "/util/opt/repeatmasker/4.0/gcc/4.4/ncbi-rmblastn-2.2.28/bin")
+
