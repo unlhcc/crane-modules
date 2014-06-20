f43fba9525a87be12bcc0f833722f598b4305f83
acaprez2 <acaprez2@unl.edu>
2014-06-20 15:19:56 -0500

    Added module for SNAP 2013-11-29.

---
+++ Core/snap/2013-11-29.lua
@@ -0,0 +1,15 @@
+help (
+[[
+This module loads snap. This module makes available the snap executables. Documentation for snap is available online at the publisher\'s website: http://korflab.ucdavis.edu/software.html.
+
+Version 2013-11-29
+]])
+
+whatis("Name: snap")
+whatis("Version: 2013-11-29")
+whatis("Category: computational biology, genomics")
+whatis("Keywords:  Biology, Genomics" )
+whatis("Description: SNAP - (Semi-HMM-based Nucleic Acid Parser) gene prediction tool.")
+whatis("URL: http://korflab.ucdavis.edu/software.html")
+prepend_path("PATH",    "/util/opt/snap/2013-11-29/gcc/4.4")
+setenv("ZOE",	"/util/opt/snap/2013-11-29/gcc/4.4/Zoe")
