8d09bb6597fd2131d2a0af18744c1ebdba8e4e0f
acaprez2 <acaprez2@unl.edu>
2014-06-23 16:44:15 -0500

    Added module for GeneMarkS 2.5p.

---
+++ Core/genemarks/2.5p.lua
@@ -0,0 +1,17 @@
+help(
+[[
+The genemarks module file defines the following environment variables:
+PATH for the location of the distribution executables.
+
+Version 2.5p
+]]
+)
+
+whatis("Name: GeneMarkS")
+whatis("Version: 2.5p")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics")
+whatis("URL: http://exon.biology.gatech.edu/GeneMark")
+whatis("Description: GeneMarkS: a self-training method for prediction of gene starts in microbial genomes. Implications for finding sequence motifs in regulatory regions")
+
+prepend_path("PATH",              "/util/opt/genemarks/2.5p/gmsuite")
