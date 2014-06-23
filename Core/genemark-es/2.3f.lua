fcfc4a84c56597f743aac5817a9e9531534cb8eb
acaprez2 <acaprez2@unl.edu>
2014-06-23 16:58:14 -0500

    Added module for GeneMark-ES 2.3f.

---
+++ Core/genemark-es/2.3f.lua
@@ -0,0 +1,17 @@
+help(
+[[
+The genemark-es module file defines the following environment variables:
+PATH for the location of the distribution executables.
+
+Version 2.3f
+]]
+)
+
+whatis("Name: GeneMark-ES")
+whatis("Version: 2.3f")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics")
+whatis("URL: http://exon.biology.gatech.edu/GeneMark")
+whatis("Description: GeneMark-ES.")
+
+prepend_path("PATH",              "/util/opt/genemark-es/2.3f/gmes")
