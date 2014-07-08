1ccd9b28cd45b1b60c62eee123bb685232472181
acaprez2 <acaprez2@unl.edu>
2014-07-08 10:25:10 -0500

    Added module for maq 0.7.

---
+++ Core/maq/0.7.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads maq built with GCC.
+This module makes available the maq executable. Documentation for maq is available online at the publisher\'s website: http://maq.sourceforge.net/index.shtml.
+
+Version 0.7
+]])
+
+whatis("Name: maq")
+whatis("Version: 0.7")
+whatis("Category: computational biology, genomics")
+whatis("Keywords:  Biology, Genomics, Alignment, Sequencing")
+whatis("Description: maq - Mapping and Assembly with Quality.")
+whatis("URL: http://maq.sourceforge.net/index.shtml")
+
+prepend_path("PATH"       ,"/util/opt/maq/0.7/gcc/4.4/bin")
