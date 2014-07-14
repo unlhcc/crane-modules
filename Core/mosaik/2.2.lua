bbd60b02c56bd0f2e6ba4429d3a13d21306d5089
acaprez2 <acaprez2@unl.edu>
2014-07-14 11:46:48 -0500

    Added module for MOSAIK 2.2.

---
+++ Core/mosaik/2.2.lua
@@ -0,0 +1,18 @@
+help(
+[[
+The mosiak module file defines the following environment variables:
+PATH for the location of the mosaik distribution executables.
+
+Version 2.2
+]]
+)
+
+whatis("Name: MOSAIK")
+whatis("Version: 2.2")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Assembly")
+whatis("URL: https://code.google.com/p/mosaik-aligner")
+whatis("Description: MOSAIK is a reference-guided aligner for next-generation sequencing technologies.")
+
+
+prepend_path("PATH",              "/util/opt/mosaik/2.2/gcc/4.4/bin")
