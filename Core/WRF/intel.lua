99975b8c2dde9e1c4cd35ed31e92c7c7103e16a5
Adam Caprez <acaprez2@unl.edu>
2014-08-04 15:00:19 -0500

    Updated comment in WRF module files.

--- Core/WRF/intel.lua
+++ Core/WRF/intel.lua
@@ -14,9 +14,10 @@ whatis("Category: library, runtime support")
 whatis("Keywords: System, Library")
 whatis("URL: http://www.wrf-model.org/index.php")
 
--- netcdf must be loaded last to make sure its 'ncdump' is first in the path (and not the hdf4 version).
--- lmod 5.4 has support for priorities for a prepend_path statement, so it's probably better to tweak the
--- netcdf module to use that once Crane is upgraded to 5.4.
+-- A priority attribute in the netcdf module prepend_path function for
+-- the PATH variable ensures that the netcdf 'ncdump' is first in
+-- the path instead of the hdf4 one.
+-- See RT #7945.
 load("compiler/intel/13","openmpi/1.6","NCL/6.1","NCO/4.4","udunits2/2.1","szip/2.1","hdf5/1.8","hdf4/4.2","netcdf/4.2")
 setenv("WRFIO_NCD_LARGE_FILE_SUPPORT", "1")
 setenv("JASPERINC", "/util/opt/jasper/1.900.1/intel/13/include")
