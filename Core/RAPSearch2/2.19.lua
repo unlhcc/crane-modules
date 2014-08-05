a671c28bf11bcc4d9c4aebe019b2003eff2c4fe9
Adam Caprez <acaprez2@unl.edu>
2014-08-05 10:47:47 -0500

    Added module for RAPSearch2 2.19.

---
+++ Core/RAPSearch2/2.19.lua
@@ -0,0 +1,19 @@
+help(
+[[
+The RAPSearch2 module defines the following environment variables:
+PATH for the location of the RAPSearch2 distribution executables.
+
+Version 2.19
+]]
+)
+
+whatis("Name: RAPSearch2")
+whatis("Version: 2.19")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Alignment")
+whatis("URL: http://sourceforge.net/projects/rapsearch2")
+whatis("Description: RAPSearch2 is a tool for fast protein similarity searches.")
+
+
+prepend_path("PATH",              "/util/opt/rapsearch2/2.19/gcc/4.4/bin")
+
