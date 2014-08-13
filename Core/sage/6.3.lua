6575ef0d1410d75acbb503549398abadd6e48b7e
Emelie Harstad <eharstad@login.crane.hcc.unl.edu>
2014-08-13 15:04:55 -0500

    Added sage 6.3 module.

---
+++ Core/sage/6.3.lua
@@ -0,0 +1,19 @@
+help(
+[[
+The sage module file defines the following environment variables:
+PATH for the location of the sage executables.
+
+Version 6.3
+]]
+)
+
+whatis("Name: Sage")
+whatis("Version: 6.3")
+whatis("Category: mathematics")
+whatis("Keywords: Mathematics")
+whatis("URL: http://www.sagemath.org/")
+whatis("Description: 'Sage is a free open-source mathematics software system licensed under the GPL. It builds on top of many existing open-source packages: NumPy, SciPy, matplotlib, Sympy, Maxima, GAP, FLINT, R and many more.' Mission: 'Creating a viable free open source alternative to Magma, Maple, Mathematica and Matlab.'") 
+
+
+prepend_path("PATH",              "/util/opt/sage/6.3/gcc/4.4/sage")
+
