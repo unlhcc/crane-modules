c6a090b16954aaf5b1fa0f3a91300eb13d65f460
Adam Caprez <acaprez2@unl.edu>
2014-09-03 15:59:13 -0500

    Added module file for Apache Maven 3.2.

---
+++ Core/maven/3.2.lua
@@ -0,0 +1,15 @@
+help (
+[[
+This module loads Apache Maven.
+
+Version 3.2.
+]])
+
+whatis("Name: maven")
+whatis("Version: 3.2")
+whatis("Category: system, build tool")
+whatis("Keywords:  system, utility, build tool")
+whatis("Description: Apache Maven is a software project management and comprehension tool.")
+whatis("URL: http://maven.apache.org/index.html")
+
+prepend_path("PATH"       ,"/util/opt/maven/3.2/bin")
