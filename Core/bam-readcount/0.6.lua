99b01b05a618bc36d224c435010b60a8d7614e69
Adam Caprez <acaprez2@unl.edu>
2014-09-04 11:47:32 -0500

    Added module file for bam-readcount 0.6.

---
+++ Core/bam-readcount/0.6.lua
@@ -0,0 +1,17 @@
+help(
+[[
+The bam-readcount module file defines the following environment variables:
+PATH for the location of the bam-readcount distribution executables.
+
+Version 0.6
+]]
+)
+
+whatis("Name: bam-readcount")
+whatis("Version: 0.6")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Sequencing")
+whatis("URL: https://github.com/genome/bam-readcount")
+whatis("Description: The purpose of this program is to generate metrics at single nucleotide positions.")
+
+prepend_path("PATH",              "/util/opt/bam-readcount/0.6/gcc/4.4/bin")
