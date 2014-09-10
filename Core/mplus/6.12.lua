d0c1cc081b0ddda8b92c67fe01801f6f74d12ddd
Adam Caprez <acaprez2@unl.edu>
2014-09-10 16:45:54 -0500

    Added module files for Mplus.

---
+++ Core/mplus/6.12.lua
@@ -0,0 +1,15 @@
+help (
+[[
+This module loads Mplus.
+
+Version 6.12.
+]])
+
+whatis("Name: Mplus")
+whatis("Version: 6.12")
+whatis("Category: Application")
+whatis("Keywords:  Application, Statistics, Math")
+whatis("Description: Mplus is a latent variable modeling program with a wide variety of analysis capabilities.")
+whatis("URL: http://www.statmodel.com")
+
+prepend_path("PATH"       ,"/util/opt/mplus/6.12")
---
+++ Core/mplus/7.0.lua
@@ -0,0 +1,15 @@
+help (
+[[
+This module loads Mplus.
+
+Version 7.0.
+]])
+
+whatis("Name: Mplus")
+whatis("Version: 7.0")
+whatis("Category: Application")
+whatis("Keywords:  Application, Statistics, Math")
+whatis("Description: Mplus is a latent variable modeling program with a wide variety of analysis capabilities.")
+whatis("URL: http://www.statmodel.com")
+
+prepend_path("PATH"       ,"/util/opt/mplus/7.0")
