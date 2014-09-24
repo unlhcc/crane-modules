a8bafebb73af6e2248b33603027446cdf25ead1a
Tom Harvill <tharvill@login.crane.hcc.unl.edu>
2014-09-24 15:06:21 -0500

    x

--- MPI/intel/13/openmpi/1.8/LIS/7.0r.lua
+++ MPI/intel/13/openmpi/1.8/LIS/7.0r.lua
@@ -14,7 +14,21 @@ whatis("Category: library, runtime support")
 whatis("Keywords: Applications, Geography, GIS ")
 whatis("URL: http://lis.gsfc.nasa.gov/")
 
+
 prepend_path("LIS_ARCH","linux_lfc");
+prepend_path("LDFLAGS","-L/util/opt/LIS/dependencies/netcdf-4.3.0/lib -lnetcdff -lnetcdf_c++ -lnetcdf -lhdf5_hl -lhdf5")
+prepend_path("LIS_HDFEOS","/util/opt/LIS/dependencies/hdfeos5.1.15/lib")
+prepend_path("CPPFLAGS","-I/util/opt/LIS/dependencies/netcdf-4.3.0/include")
+prepend_path("LIS_LIBESMF","/util/opt/LIS/dependencies/esmf/lib/lib0/Linux.intel.64.openmpi.default")
+prepend_path("LIS_HDF5","/util/opt/LIS/dependencies/hdf5-1.8.12/lib")
+prepend_path("LIS_HDF4","/util/opt/LIS/dependencies/hdf4.2r4/lib")
+prepend_path("LIS_CC","mpicc")
+prepend_path("LIS_JASPER","/util/opt/LIS/dependencies/jasper-1.900.1/lib")
+prepend_path("LIS_MODESMF","/util/opt/LIS/dependencies/esmf/mod/mod0/Linux.intel.64.openmpi.default")
+prepend_path("LIS_GRIBAPI","/util/opt/LIS/dependencies/grib_api-1.9.16/lib")
+prepend_path("LIS_NETCDF","/util/opt/LIS/dependencies/netcdf-4.3.0/lib")
+prepend_path("LIS_FC","mpif90")
+
 prepend_path("PATH","/util/opt/LIS/7.0r/exec")
 prepend_path("LD_LIBRARY_PATH","/util/opt/LIS/dependencies/netcdf-4.3.0/lib")
 prepend_path("LD_LIBRARY_PATH","/util/opt/LIS/dependencies/jasper-1.900.1/lib")
