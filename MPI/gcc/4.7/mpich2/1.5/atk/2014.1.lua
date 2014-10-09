4702da83071e9a51d62383eec123d751fabe47f2
Jun Wang <jwang@login.crane.hcc.unl.edu>
2014-10-09 12:51:58 -0500

    renamed:    MPI/gcc/4.7/mpich2/1.5/atk/13.8.1.lua -> MPI/gcc/4.7/mpich2/1.5/atk/2014.1.lua 	modified:   crane_modules.xml

--- MPI/gcc/4.7/mpich2/1.5/atk/2014.1.lua
+++ MPI/gcc/4.7/mpich2/1.5/atk/2014.1.lua
@@ -1,5 +1,5 @@
 local help_message = [[
-ATK 13.8.1
+ATK 2014.1
 
 This module loads the ATK environment, prebuilt with MPICH2
 
@@ -8,7 +8,7 @@ This module loads the ATK environment, prebuilt with MPICH2
 help(help_message,"\n")
 
 whatis("Name: ATK")
-whatis("Version: 13.8.1")
+whatis("Version: 2014.1")
 whatis("Category: Application, Chemistry")
 whatis("Keywords: Application, Chemistry")
 whatis("URL: http://www.quantumwise.com/")
@@ -16,10 +16,10 @@ whatis("URL: http://www.quantumwise.com/")
 setenv("QUANTUM_LICENSE_PATH", "6200@dmol4b.unl.edu")
 setenv("LMX_LICENSE_PATH", "6200@dmol4b.unl.edu")
 
-prepend_path("PATH","/util/opt/atk/13.8.1/vnl/bin:/util/opt/atk/13.8.1/atkpython/bin:/util/opt/atk/13.8.1/license")
+prepend_path("PATH","/util/opt/atk/2014.1/bin:/util/opt/atk/2014.1/license")
 
 -- Workaround for RHEL 6.5 upgrade
 -- See http://quantumwise.com/forum/index.php?topic=2564.15#.U-zVMlb9O0s
-setenv("LD_PRELOAD",	"libstdc++.so.6")
+-- setenv("LD_PRELOAD",	"libstdc++.so.6")
 
 family("atk")
