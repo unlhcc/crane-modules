7da8e6600c83e123e2be96b3a182465fa0d7699b
Adam Caprez <acaprez2@unl.edu>
2014-11-04 14:51:21 -0600

    Add module for ASReml 3.0

---
+++ Core/asreml/3.0.lua
@@ -0,0 +1,15 @@
+help (
+[[
+This module loads ASReml.
+Version 3.0.
+]])
+
+whatis("Name: Asreml")
+whatis("Version: 3.0")
+whatis("Category: data analysis, statistics")
+whatis("Keywords: Data Analysis, Statistics")
+whatis("Description: ASReml")
+whatis("URL: http://www.vsni.co.uk/software/asreml")
+
+prepend_path("PATH"		,"/util/opt/asreml/3.0/bin")
+setenv("ASREML_LICENSE_FILE"	,"/util/opt/asreml/3.0/bin/asreml.lic")
