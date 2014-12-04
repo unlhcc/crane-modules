63ef65f2af84c7a097a146cfe098614bd86789f2
Adam Caprez <acaprez2@unl.edu>
2014-12-04 09:59:47 -0600

    Add module file for gnuplot 4.6

---
+++ Core/gnuplot/4.6.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads gnuplot.
+
+Version 4.6.
+]])
+
+whatis("Name: gnuplot")
+whatis("Version: 4.6")
+whatis("Category: Application")
+whatis("Keywords:  Application, Graphing")
+whatis("Description: Gnuplot is a portable command-line driven graphing utility for Linux, OS/2, MS Windows, OSX, VMS, and many other platforms.")
+whatis("URL: http://www.gnuplot.info")
+
+prepend_path("PATH"       ,"/util/opt/gnuplot/4.6/gcc/4.4/bin")
+prepend_path("MANPATH"	  ,"/util/opt/gnuplot/4.6/gcc/4.4/share/man")
