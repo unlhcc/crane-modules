76691d0c1907a6924d98accf21f20ccc6dfaf675
Adam Caprez <acaprez2@unl.edu>
2015-01-08 09:57:32 -0600

    Add module file Cufflinks 2.2

---
+++ Core/cufflinks/2.2.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads the cufflinks software package.
+Documentation for cufflinks is available online at the publisher website: http://cufflinks.cbcb.umd.edu/
+
+Version 2.2
+]])
+
+whatis("Name: cufflinks")
+whatis("Version: 2.2")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, RNAseq, Transcriptome profiling")
+whatis("Description: cufflinks - Transcript assembly, differential expression, and differential regulation for RNA-Seq")
+whatis("URL: http://cufflinks.cbcb.umd.edu/")
+
+prepend_path("PATH",              "/util/opt/cufflinks/2.2/bin")
