41c5993d28344b42f45136b0e93e7311f241fa7b
Jean-Jack M. Riethoven <jeanjack@unl.edu>
2015-02-05 12:07:27 -0600

    Renamed lua according to guidelines; recompiled and restaged Phrap to be in line with hmmer's gcc version.

--- Core/trf/4.0.lua
+++ Core/trf/4.0.lua
@@ -14,5 +14,5 @@ whatis("Keywords: Biology, Genomics")
 whatis("URL: http://tandem.bu.edu/trf/trf.html")
 whatis("Description: Tandem Repeats Finder is a program to locate and display tandem repeats in DNA sequences.")
 
-prepend_path("PATH",              "/util/opt/BCRF/trf/4.07b")
+prepend_path("PATH",              "/util/opt/BCRF/trf/4.0/gcc/4.4/bin")
 
