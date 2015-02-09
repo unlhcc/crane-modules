1404ebaf77eb5595a6dd0c4c403a610681251c7a
JingchaoZhang <zhang@unl.edu>
2015-02-09 16:06:16 -0600

    ESMF without MPI for NUWRF

---
+++ Compiler/intel/13/esmf/5.2.lua
@@ -0,0 +1,24 @@
+local help_message = [[
+ESMF 5.2.0rp3 without MPI support
+
+This module loads the ESMF, built with the Intel 
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: ESMF")
+whatis("Version: 5.2.0rp3")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.earthsystemmodeling.org/")
+
+prepend_path{"PATH",                "/util/opt/ESMF/5.2.0rp3/intel/13/bin",priority=100}
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/ESMF/5.2.0rp3/intel/13/lib")
+prepend_path("MANPATH",             "/util/opt/ESMF/5.2.0rp3/intel/13/doc")
+prepend_path("INCLUDE",             "/util/opt/ESMF/5.2.0rp3/intel/13/include")
+setenv("ESMF_NOMPI_LIB", "/util/opt/ESMF/5.2.0rp3/intel/13/lib/libO/Linux.intel.64.mpiuni.default") 
+setenv("ESMF_NOMPI_MOD", "/util/opt/ESMF/5.2.0rp3/intel/13/mod/modO/Linux.intel.64.mpiuni.default")
+
+family("esmf")
