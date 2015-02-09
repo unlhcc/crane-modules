20a996f2ba5b7e1847cc4090db384bb362169e8b
JingchaoZhang <zhang@unl.edu>
2015-02-09 17:25:45 -0600

    grib-api for NUWRF

---
+++ Compiler/intel/13/grib-api/1.12.lua
@@ -0,0 +1,25 @@
+local help_message = [[
+GRIB-API 1.12.3
+
+This module loads the GRIB-API, built with the Intel 
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: GRIB-API")
+whatis("Version: 1.12.3")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: https://software.ecmwf.int/wiki/display/GRIB/Home")
+
+prepend_path{"PATH",                "/util/opt/grib-api/1.12.3/intel/13/bin",priority=100}
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/grib-api/1.12.3/intel/13/lib")
+prepend_path("MANPATH",             "/util/opt/grib-api/1.12.3/intel/13/share/grib_api")
+prepend_path("INCLUDE",             "/util/opt/grib-api/1.12.3/intel/13/include")
+setenv("GRIBAPI", "/util/opt/grib-api/1.12.3/intel/13") 
+setenv("GRIBAPI_INCLUDE", "/util/opt/grib-api/1.12.3/intel/13/include")
+setenv("GRIBAPI_LIB", "/util/opt/grib-api/1.12.3/intel/13/lib")
+
+family("grib-api")
