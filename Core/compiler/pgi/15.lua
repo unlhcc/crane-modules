733f1f3649a9bff31c795ab0abc05a152c36f4ed
Adam Caprez <acaprez2@unl.edu>
2015-02-25 13:44:05 -0600

    Add PGI 15 compiler module file, change default

---
+++ Core/compiler/pgi/15.lua
@@ -0,0 +1,31 @@
+local help_message = [[
+PGI Compilers 15
+
+This module loads PGI Compiler variables.
+The command directory is added to PATH.
+The library directory is added to LD_LIBRARY_PATH.
+The include directory is added to INCLUDE.
+The man     directory is added to MANPATH.
+]]
+
+help(help_message,"\n")
+
+whatis("Name: PGI Compilers")
+whatis("Version: 15")
+whatis("Category: compiler")
+whatis("Keywords: System, compiler")
+whatis("URL: http://www.pgroup.com")
+
+prepend_path("PATH",                "/util/comp/pgi/2015/linux86-64/15/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/comp/pgi/2015/linux86-64/15/lib")
+prepend_path("MANPATH",             "/util/comp/pgi/2015/linux86-64/15/man")
+prepend_path("INCLUDE",             "/util/comp/pgi/2015/linux86-64/15/include")
+prepend_path("MODULEPATH",          "/util/opt/modulefiles/Compiler/pgi/15")
+setenv("CC",	"pgcc")
+setenv("FC",	"pgf90")
+setenv("F90",	"pgf90")
+setenv("F77",	"pgf77")
+setenv("CPP",	"pgcc -E")
+setenv("CXX",	"pgcpp")
+
+family("compiler")
