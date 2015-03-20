e5ae921b8b218c67f0e429b0f46e63acaaa3204f
Adam Caprez <acaprez2@unl.edu>
2015-03-20 16:35:52 -0500

    Add tabix 0.2 module file

---
+++ Core/tabix/0.2.lua
@@ -0,0 +1,18 @@
+help (
+[[
+The tabix module file defines the following environment variables:
+PATH for the location of the tabix distribution binary. 
+
+Version 0.2
+
+]])
+
+whatis("Name: tabix")
+whatis("Version: 0.2")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Alignment, Sequencing")
+whatis("URL: http://samtools.sourceforge.net/tabix.shtml")
+whatis("Description: Tabix indexes a TAB-delimited genome position file in.tab.bgz and creates an index file")
+
+prepend_path("PATH"       ,"/util/opt/tabix/0.2/gcc/4.4/bin")
+
