1b4ed4bd83ef44fe5ed42d89bf57af4770b9a1e4
Adam Caprez <acaprez2@unl.edu>
2015-03-30 15:11:00 -0500

    Add module file for Mauve 2.4

---
+++ Core/mauve/2.4.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module makes available the mauve executable. Documentation for mauve is available online at the publisher\'s website: http://darlinglab.org/mauve/mauve.html.
+
+Version 2.4
+]])
+
+whatis("Name: mauve")
+whatis("Version: 2.4")
+whatis("Category: computational biology, genomics")
+whatis("Keywords:  Biology, Genomics, Alignment, Sequencing")
+whatis("Description: mauve - Multiple Genome Alignment.")
+whatis("URL: http://darlinglab.org/mauve/mauve.html")
+
+prepend_path("PATH"       ,"/util/opt/mauve/2.4")
+prepend_path("PATH"       ,"/util/opt/mauve/2.4/linux-x64")
