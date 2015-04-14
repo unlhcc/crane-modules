9793b4eb95d1b603b1dd337bc93f7cfdce5ff64d
Adam Caprez <acaprez2@unl.edu>
2015-04-14 10:26:51 -0500

    Revert "Updated printseq to bug-fixed version 0.20.4"
    Updated prinseq version already installed.
    This reverts commit 6d3e64a8d73c2d26c5af702bc5f89a888c655800.

--- Core/prinseq-lite/0.20.lua
+++ Core/prinseq-lite/0.20.lua
@@ -1,17 +1,17 @@
 help(
 [[
-PRINSEQ can be used to filter, reformat, or trim your genomic and metagenomic sequence data.
+The prinseq module file defines the following environment variables:
+PATH for the location of the prinseq distribution executables.
 
-Version 0.20.4
+Version 0.20
 ]]
 )
 
 whatis("Name: prinseq")
-whatis("Version: 0.20.4")
+whatis("Version: 0.20")
 whatis("Category: computational biology, genomics")
 whatis("Keywords: Biology, Genomics, Sequencing")
 whatis("URL: http://prinseq.sourceforge.net")
 whatis("Description: PRINSEQ can be used to filter, reformat, or trim your genomic and metagenomic sequence data.")
 
-prepend_path("PATH",              "/util/opt/BCRF/prinseq/0.20/bin")
-prepend_path("PERL5LIB",          "/util/opt/BCRF/prinseq/0.20/share/perl5:/util/opt/BCRF/prinseq/0.20/lib64/perl5:/util/opt/BCRF/prinseq/0.20/lib/perl5")
+prepend_path("PATH",              "/util/opt/prinseq/0.20/bin")
