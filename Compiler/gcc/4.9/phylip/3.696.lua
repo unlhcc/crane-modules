82285c66b3930d1c96d226e00aea6507e2912145
Seongil Eyun <eyun@login.crane.hcc.unl.edu>
2015-05-07 16:24:01 -0500

    Phylip

---
+++ Compiler/gcc/4.9/phylip/3.696.lua
@@ -0,0 +1,16 @@
+help(
+[[
+PHYLIP is a free package of programs for inferring phylogenies. It is distributed as source code, documentation files, and a number of different types of executables. These Web pages, by Joe Felsenstein of the Department of Genome Sciences and the Department of Biology at the University of Washington, contain information on PHYLIP and ways to transfer the executables, source code and documentation to your computer.
+
+Version 3.696
+]]
+)
+
+whatis("Name: PHYLIP")
+whatis("Version: 3.696")
+whatis("Category: computational biology")
+whatis("Keywords: Biology, Evolution, Phylogeny")
+whatis("URL: http://evolution.genetics.washington.edu/phylip/")
+whatis("Description: PHYLIP (the PHYLogeny Inference Package) is a package of programs for inferring phylogenies (evolutionary trees).")
+
+prepend_path("PATH",              "/util/opt/BCRF/phylip/3.696/gcc/4.9/bin")
