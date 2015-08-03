430c98ff477e3c5d6f00c508a00f4e8129035f19
Adam Caprez <acaprez2@unl.edu>
2015-08-03 10:57:18 -0500

    Add OMPI 1.8 build of Ray

---
+++ MPI/gcc/4.8/openmpi/1.8/ray/2.3.lua
@@ -0,0 +1,17 @@
+help (
+[[
+This module loads Ray, built with GCC 4.8.
+This module makes available the following executables:
+Ray
+
+Version 2.3
+]])
+
+whatis("Name: Ray")
+whatis("Version: 2.3")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Bioinformatics, Genomics","Assembly")
+whatis("Description: Ray -- Parallel genome assemblies for parallel DNA sequencing.")
+whatis("URL: http://denovoassembler.sourceforge.net/index.html")
+
+prepend_path("PATH"    ,"/util/opt/ray/2.3/openmpi/1.8/gcc/4.8")
