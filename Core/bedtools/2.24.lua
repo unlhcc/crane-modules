5c93e6394224f1d5f990dedb2b1d6ea78a0672ed
Adam Caprez <acaprez2@unl.edu>
2015-08-27 16:26:09 -0500

    Add module file for Bedtools

---
+++ Core/bedtools/2.24.lua
@@ -0,0 +1,17 @@
+help(
+[[
+The bedtools module file defines the following environment variables:
+PATH for the location of the bedtools distribution executables.
+
+Version 2.24
+]]
+)
+
+whatis("Name: bedtools")
+whatis("Version: 2.24")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Sequencing")
+whatis("URL: http://bedtools.readthedocs.org")
+whatis("Description: bedtools: a powerful toolset for genome arithmetic.")
+
+prepend_path("PATH",              "/util/opt/bedtools/2.24/gcc/4.4/bin")
