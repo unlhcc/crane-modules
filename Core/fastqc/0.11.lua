eb271c57780a70b2f058b6738fc44a161f730613
Jean-Jack M. Riethoven <jeanjack@unl.edu>
2015-09-08 15:44:28 -0500

    Updated FastQC to 0.11.3

---
+++ Core/fastqc/0.11.lua
@@ -0,0 +1,14 @@
+help (
+[[
+This module loads fastqc. This module makes available the fastqc executables. Documentation for fastqc is available online at the publisher\'s website: http://www.bioinformatics.babraham.ac.uk/projects/fastqc/
+
+Version 0.11.3
+]])
+
+whatis("Name: fastqc")
+whatis("Version: 0.11.3")
+whatis("Category: computational biology, genomics")
+whatis("Keywords:  Biology, Genomics, Sequencing, FastQ, Quality Control")
+whatis("Description: fastqc - A Quality Control application for FastQ files")
+whatis("URL: http://www.bioinformatics.babraham.ac.uk/projects/fastqc/")
+prepend_path("PATH",    "/util/opt/fastqc/0.11")
