bae93e8c43d54d700699428b1df0087163cd5d68
Jean-Jack M. Riethoven <jeanjack@unl.edu>
2015-09-09 11:09:02 -0500

    Added ea-utils (fast and lightweight native C++ next-gen sequencing processing and info tools).

---
+++ Core/ea-utils/1.0.lua
@@ -0,0 +1,16 @@
+help(
+[[
+EA Utils is a collection of command line tools for processing NextGen sequencing data. Included are fastq-stats and sam-stats which are lightweight and fast tools that display a variety of statistics on fastq and sam files.
+
+Version 1.01
+]]
+)
+
+whatis("Name: ea-utils")
+whatis("Version: 1.01")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Quality Control, Utility, Sequencing")
+whatis("URL: https://code.google.com/p/ea-utils/")
+whatis("Description: EA Utils is a collection of command line tools for processing NextGen sequencing data. Included are fastq-stats and sam-stats which are lightweight tools that display a variety of statistics on fastq and sam files.")
+
+prepend_path("PATH",              "/util/opt/BCRF/ea-utils/1.0/gcc/4.9/bin")
