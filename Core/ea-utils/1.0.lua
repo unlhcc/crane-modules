3c8fa25c6b66824540a69bb5de5dc186b51a552d
Jean-Jack M. Riethoven <jeanjack@unl.edu>
2015-09-09 11:41:48 -0500

    Fixed path - was compiled by default compiler.

--- Core/ea-utils/1.0.lua
+++ Core/ea-utils/1.0.lua
@@ -13,4 +13,4 @@ whatis("Keywords: Biology, Genomics, Quality Control, Utility, Sequencing")
 whatis("URL: https://code.google.com/p/ea-utils/")
 whatis("Description: EA Utils is a collection of command line tools for processing NextGen sequencing data. Included are fastq-stats and sam-stats which are lightweight tools that display a variety of statistics on fastq and sam files.")
 
-prepend_path("PATH",              "/util/opt/BCRF/ea-utils/1.0/gcc/4.9/bin")
+prepend_path("PATH",              "/util/opt/BCRF/ea-utils/1.0/gcc/4.4/bin")
