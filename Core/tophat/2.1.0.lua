420b5ae8752665f62a8e640e1c122c27e65d4e06
JingchaoZhang <zhang@unl.edu>
2015-10-05 10:44:26 -0500

    updated Tophat 2.1.0 on Crane

---
+++ Core/tophat/2.1.0.lua
@@ -0,0 +1,19 @@
+help(
+[[
+The tophat module file defines the following environment variables:
+PATH for the location of the tophat executables.
+
+Version 2.1.0
+]]
+)
+
+whatis("Name: TopHat")
+whatis("Version: 2.1.0")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, RNAseq, Transcriptome profiling, Alignment")
+whatis("URL: http://tophat.cbcb.umd.edu/")
+whatis("Description: TopHat2 is a fast splice junction mapper for RNA-Seq reads. It aligns RNA-Seq reads to mammalian-sized genomes using the ultra high-throughput short read aligner Bowtie, and then analyzes the mapping results to identify splice junctions between exons.")
+
+prepend_path("PATH",              "/util/opt/tophat/2.1/bin")
+
+prereq ("bowtie")
