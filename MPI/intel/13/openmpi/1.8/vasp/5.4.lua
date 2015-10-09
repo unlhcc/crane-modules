9d8dfd41c002c4f48cab7e2273c34f9d0aee9d22
Jun Wang <jwang@login.crane.hcc.unl.edu>
2015-10-09 16:58:55 -0500

    renamed:    MPI/intel/13/openmpi/1.8/vasp/5.4.1.lua -> MPI/intel/13/openmpi/1.8/vasp/5.4.lua 	modified:   crane_modules.xml

--- MPI/intel/13/openmpi/1.8/vasp/5.4.lua
+++ MPI/intel/13/openmpi/1.8/vasp/5.4.lua
@@ -1,5 +1,5 @@
 local help_message = [[
-VASP 5.4.1
+VASP 5.4
 
 This module loads the vasp environment, built with the intel 13
 compilers. Only authorized users are able to run vasp.
@@ -9,11 +9,11 @@ compilers. Only authorized users are able to run vasp.
 help(help_message,"\n")
 
 whatis("Name: VASP")
-whatis("Version: 5.4.1")
+whatis("Version: 5.4")
 whatis("Category: library, runtime support")
 whatis("Keywords: Applications, Chemistry")
 whatis("URL: https://www.vasp.at/")
 
-prepend_path("PATH","/util/opt/vasp/5.4.1/bin")
+prepend_path("PATH","/util/opt/vasp/5.4/bin")
 
 family("vasp")
