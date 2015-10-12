c93199ac940f284cad841ad8091298c140edfdfb
JingchaoZhang <zhang@unl.edu>
2015-10-12 10:08:32 -0500

    kallisto 0.42.3 on Crane

---
+++ Compiler/gcc/4.8/kallisto/0.42.lua
@@ -0,0 +1,19 @@
+local help_message = [[
+kallisto 0.42.3
+
+This module loads the kallisto, built with the GCC 
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: kallisto")
+whatis("Version: 0.42.3")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: https://github.com/pachterlab/kallisto")
+
+prepend_path("PATH",                "/util/opt/kallisto/0.42/gcc/4.8/bin/")
+
+family("kallisto")
