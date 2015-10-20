b8a21487265a200ebed786cf029e5fb79a4c2bb1
Seongil Eyun <eyun@login.crane.hcc.unl.edu>
2015-10-19 20:24:49 -0500

    allpathslg

---
+++ Compiler/gcc/4.7/allpathslg/latest.lua
@@ -0,0 +1,19 @@
+local help_message = [[
+ALLPATHS-LG
+ 
+This module loads ALLPATHS-LG.
+ 
+]]
+ 
+help(help_message,"\n")
+ 
+whatis("Name: ALLPATHS-LG")
+whatis("Version: Latest")
+whatis("Category: biology, genomics")
+whatis("Keywords: Biology, Genomics, Sequencing")
+whatis("URL: http://www.broadinstitute.org/software/allpaths-lg/blog")
+ 
+prepend_path("PATH",                "/util/opt/BCRF/allpathslg/lastest/gcc/4.7/bin")
+~
+~
+~
