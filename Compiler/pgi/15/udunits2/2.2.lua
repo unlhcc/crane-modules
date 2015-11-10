097b0778b54e6477ace31cb4922acfd06f4909d6
JingchaoZhang <zhang@unl.edu>
2015-11-10 12:01:03 -0600

    udunits 2.2 on Crane

---
+++ Compiler/pgi/15/udunits2/2.2.lua
@@ -0,0 +1,22 @@
+local help_message = [[
+udunits 2.2
+
+This module loads the udunits2 package, built with the PGI
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: udunits2")
+whatis("Version: 2.2")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.unidata.ucar.edu/software/udunits")
+
+prepend_path("PATH",		    "/util/opt/udunits/2.2/pgi/15/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/udunits/2.2/pgi/15/lib")
+prepend_path("MANPATH",             "/util/opt/udunits/2.2/pgi/15/share")
+prepend_path("INCLUDE",             "/util/opt/udunits/2.2/pgi/15/include")
+
+family("udunits2")
