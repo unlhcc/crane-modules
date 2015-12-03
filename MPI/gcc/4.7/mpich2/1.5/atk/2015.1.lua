3cc6216c1fb4440b5a3ef8b33ba94802c63be021
Jun Wang <jwang@login.crane.hcc.unl.edu>
2015-12-03 08:56:09 -0600

    renamed:    MPI/gcc/4.7/mpich2/1.5/atk/2015.0.lua -> MPI/gcc/4.7/mpich2/1.5/atk/2015.1.lua 	modified:   crane_modules.xml

--- MPI/gcc/4.7/mpich2/1.5/atk/2015.1.lua
+++ MPI/gcc/4.7/mpich2/1.5/atk/2015.1.lua
@@ -1,5 +1,5 @@
 local help_message = [[
-ATK 2015.0
+ATK 2015.1
 
 This module loads the ATK environment, prebuilt with MPICH2
 
@@ -8,7 +8,7 @@ This module loads the ATK environment, prebuilt with MPICH2
 help(help_message,"\n")
 
 whatis("Name: ATK")
-whatis("Version: 2015.0")
+whatis("Version: 2015.1")
 whatis("Category: Application, Chemistry")
 whatis("Keywords: Application, Chemistry")
 whatis("URL: http://www.quantumwise.com/")
@@ -16,7 +16,7 @@ whatis("URL: http://www.quantumwise.com/")
 setenv("QUANTUM_LICENSE_PATH", "6200@dmol4b.unl.edu")
 setenv("LMX_LICENSE_PATH", "6200@dmol4b.unl.edu")
 
-prepend_path("PATH","/util/opt/atk/2015.0/bin:/util/opt/atk/2015.0/license")
+prepend_path("PATH","/util/opt/atk/2015.1/bin:/util/opt/atk/2015.1/license")
 
 -- Workaround for RHEL 6.5 upgrade
 -- See http://quantumwise.com/forum/index.php?topic=2564.15#.U-zVMlb9O0s
