1992ac2c65e2693c63f7cf3729d0226b5821f03c
JingchaoZhang <zhang@unl.edu>
2015-12-16 17:10:07 -0600

    BICseq 1.1.2 on Crane

---
+++ Core/BICseq/1.1.lua
@@ -0,0 +1,19 @@
+help (
+[[
+BICseq Copy Number analysis from Whole-genome Sequencing data
+
+Version 1.1.2
+]])
+
+whatis("Name: BICseq")
+whatis("Version: 1.1.2")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Sequencing, Assembly")
+whatis("Description: BIC-seq can accurately and efficiently identify CNVs via minimizing the Bayesian information criterion.")
+whatis("URL: http://compbio.med.harvard.edu/Supplements/PNAS11.html")
+
+prepend_path("PATH"                     ,"/util/opt/BICseq/1.1/gcc/4.4/BICseq/PERL_pipeline/BICseq_1.1.2/BIC-seq")
+prepend_path("PATH"                     ,"/util/opt/BICseq/1.1/gcc/4.4/BICseq/PERL_pipeline/BICseq_1.1.2/BIC-seq/BICseq")
+prepend_path("PATH"                     ,"/util/opt/BICseq/1.1/gcc/4.4/BICseq/PERL_pipeline/BICseq_1.1.2/SAMgetUnique/samtools-0.1.7a_getUnique-0.1.1")
+prepend_path("LD_LIBRARY_PATH"          ,"/util/opt/BICseq/1.1/gcc/4.4/BICseq/PERL_pipeline/BICseq_1.1.2/SAMgetUnique/samtools-0.1.7a_getUnique-0.1.1")
+
