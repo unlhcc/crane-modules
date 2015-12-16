36a892e392e90198dfbbd78648268f5a3c5d780e
JingchaoZhang <zhang@unl.edu>
2015-12-16 15:12:45 -0600

    CNVnator 0.3 on Crane

---
+++ Compiler/gcc/4.9/CNVnator/0.3.lua
@@ -0,0 +1,18 @@
+help(
+[[
+CNVnator is a tool for CNV discovery and genotyping from depth-of-coverage by mapped reads
+
+Version 0.3
+]]
+)
+
+whatis("Name: CNVnator")
+whatis("Version: 0.3")
+whatis("Category: genotyping")
+whatis("Keywords: Library")
+whatis("URL: https://github.com/abyzovlab/CNVnator")
+whatis("Description: CNVnator: an approach to discover, genotype, and characterize typical and atypical CNVs from family and population genome sequencing.")
+
+
+prepend_path("PATH",                    "/util/opt/CNVnator/0.3/gcc/4.9/bin")
+
