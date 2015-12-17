7b1c28289eb0bedf70f39bfa6265e71d2f8a8908
JingchaoZhang <zhang@unl.edu>
2015-12-17 13:10:34 -0600

    Bio-Samtools 1.38 on Crane

---
+++ Core/biosamtools/1.38.lua
@@ -0,0 +1,19 @@
+help (
+[[
+Binder of samtools for ruby, on the top of FFI. This project was born from the need to add support of BAM files to the gee_fu genome browser 
+
+Version 1.38
+]])
+
+whatis("Name: Bio-Samtools")
+whatis("Version: 1.38")
+whatis("Category: computational biology, genomics")
+whatis("Keywords:  Biology, Genomics")
+whatis("URL: https://www.sharcnet.ca/help/index.php/BIO-SAMTOOLS")
+
+
+prepend_path("PATH"		,"/util/opt/biosamtools/1.38/gcc/4.4/bin")
+prepend_path("MANPATH"		,"/util/opt/biosamtools/1.38/gcc/4.4/man")
+prepend_path("PERL5LIB"		,"/util/opt/biosamtools/1.38/gcc/4.4/lib/perl5/x86_64-linux-thread-multi/Bio/DB")
+prepend_path("PERL5LIB"         ,"/util/opt/biosamtools/1.38/gcc/4.4/lib/perl5")
+
