4f726347b5bdc4ab4e77e7d3949a4921d8c0ca4c
JingchaoZhang <zhang@unl.edu>
2015-12-17 13:25:50 -0600

    crest 1.0 on Crane

---
+++ Core/crest/1.0.lua
@@ -0,0 +1,17 @@
+help (
+[[
+CREST (Clipping Reveals Structure) is a new algorithm for detecting genomic structural variations at base-pair resolution using next-generation sequencing data. 
+
+Version 1.0
+]])
+
+whatis("Name: CREST")
+whatis("Version: 1.0")
+whatis("Category: computational biology, genomics")
+whatis("Keywords:  Biology, Genomics")
+whatis("URL: http://www.stjuderesearch.org/site/lab/zhang")
+
+
+prepend_path("PATH"		,"/util/opt/crest/1.0")
+prepend_path("PERL5LIB"		,"/util/opt/crest/1.0")
+
