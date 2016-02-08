433bbc3bc9295a8a340faca7c76b16cb240c418c
JingchaoZhang <zhang@unl.edu>
2016-02-08 12:10:52 -0600

    glog 0.3.3 on Crane

---
+++ Core/glog/0.3.lua
@@ -0,0 +1,15 @@
+local help_message = [[
+glog-0.3.3
+C++ implementation of the Google logging module
+]]
+
+help(help_message,"\n")
+
+whatis("Name: glog")
+whatis("Version: 0.3.3")
+whatis("Category: Utility")
+whatis("Keywords: System, utility")
+whatis("URL: https://github.com/google/glog")
+
+prepend_path("LD_LIBRARY_PATH",		"/util/opt/glog/0.3/gcc/4.4/lib")
+prepend_path("INCLUDE",			"/util/opt/glog/0.3/gcc/4.4/include")
