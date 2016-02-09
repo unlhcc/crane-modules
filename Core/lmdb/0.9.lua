e8c3c04cba7ceb3c0391a81710ab094d742f2cd7
JingchaoZhang <zhang@unl.edu>
2016-02-09 16:25:45 -0600

    lmdb v0.9 on Crane

---
+++ Core/lmdb/0.9.lua
@@ -0,0 +1,18 @@
+help (
+[[
+Lightning memory-mapped database library
+
+Version 0.9
+]])
+
+whatis("Name: lmdb")
+whatis("Version: 0.9")
+whatis("Category: library")
+whatis("Keywords: library")
+whatis("Description: Lightning memory-mapped database library.")
+whatis("URL: https://github.com/LMDB/lmdb")
+
+prepend_path("PATH",			"/util/opt/lmdb/0.9/gcc/4.4/bin")
+prepend_path("LD_LIBRARY_PATH",		"/util/opt/lmdb/0.9/gcc/4.4/lib")
+prepend_path("INCLUDE",			"/util/opt/lmdb/0.9/gcc/4.4/include")
+
