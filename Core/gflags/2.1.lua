6c0fea0742c3a17ed40e573cd207fbff686495a8
JingchaoZhang <zhang@unl.edu>
2016-02-09 16:02:27 -0600

    gflags v2.1 on Crane

---
+++ Core/gflags/2.1.lua
@@ -0,0 +1,18 @@
+help (
+[[
+The gflags package contains a C++ library that implements commandline flags processing. It includes built-in support for standard types such as string and the ability to define flags in the source file in which they are used.
+
+Version 2.1
+]])
+
+whatis("Name: gflags")
+whatis("Version: 2.1")
+whatis("Category: library")
+whatis("Keywords: commandline flags processing")
+whatis("Description: Online documentation available at: https://gflags.github.io/gflags/")
+whatis("URL: https://github.com/gflags/gflags")
+
+prepend_path("PATH",			"/util/opt/gflags/2.1/gcc/4.4/bin")
+prepend_path("LD_LIBRARY_PATH",		"/util/opt/gflags/2.1/gcc/4.4/lib")
+prepend_path("INCLUDE",			"/util/opt/gflags/2.1/gcc/4.4/include")
+
