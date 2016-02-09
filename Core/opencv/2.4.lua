ffe9d24ebc34a329f762a048e5e5f9d41dc2be72
JingchaoZhang <zhang@unl.edu>
2016-02-09 15:47:59 -0600

    OpenCV 2.4 on Crane

---
+++ Core/opencv/2.4.lua
@@ -0,0 +1,18 @@
+help (
+[[
+OpenCV (Open Source Computer Vision) is a library of programming functions mainly aimed at real-time computer vision
+
+Version 2.4
+]])
+
+whatis("Name: OpenCV")
+whatis("Version: 2.4.11")
+whatis("Category: library")
+whatis("Keywords: Image Processing")
+whatis("Description: If the library finds Intel's Integrated Performance Primitives on the system, it will use these proprietary optimized routines to accelerate itself.")
+whatis("URL: http://opencv.org/")
+
+prepend_path("PATH",			"/util/opt/opencv/2.4/gcc/4.4/bin")
+prepend_path("LD_LIBRARY_PATH",		"/util/opt/opencv/2.4/gcc/4.4/lib")
+prepend_path("INCLUDE",			"/util/opt/opencv/2.4/gcc/4.4/include")
+
