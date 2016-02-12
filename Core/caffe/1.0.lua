97315322c987dd6abb5d51889b11f85a3a8fe0db
JingchaoZhang <zhang@unl.edu>
2016-02-12 11:24:54 -0600

    Caffe 1.0 installtion on Crane

---
+++ Core/caffe/1.0.lua
@@ -0,0 +1,18 @@
+help (
+[[
+Caffe is a deep learning framework made with expression, speed, and modularity in mind.
+
+Version 1.0
+]])
+
+whatis("Name: Caffe")
+whatis("Version: 1.0")
+whatis("Category: package")
+whatis("Keywords: deep learning")
+whatis("Description: It is developed by the Berkeley Vision and Learning Center (BVLC) and by community contributors. Yangqing Jia created the project during his PhD at UC Berkeley. Caffe is released under the BSD 2-Clause license.")
+whatis("URL: http://caffe.berkeleyvision.org/")
+
+prepend_path("PATH",			"/util/opt/caffe/1.0/gcc/4.4/bin")
+prepend_path("LD_LIBRARY_PATH",		"/util/opt/caffe/1.0/gcc/4.4/lib")
+prepend_path("INCLUDE",			"/util/opt/caffe/1.0/gcc/4.4/include")
+
