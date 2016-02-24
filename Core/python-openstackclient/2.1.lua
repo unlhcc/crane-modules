4f24c4c4f54aa0fde363084cd705934a19d92ef1
Adam Caprez <acaprez2@unl.edu>
2016-02-24 13:40:28 -0600

    Fix path

--- Core/python-openstackclient/2.1.lua
+++ Core/python-openstackclient/2.1.lua
@@ -11,7 +11,7 @@ whatis("Keywords: openstack")
 whatis("Description: OpenStackClient (aka OSC) is a command-line client for OpenStack that brings the command set for Compute, Identity, Image, Object Store and Block Storage APIs together in a single shell with a uniform command structure.")
 whatis("URL: http://docs.openstack.org/developer/python-openstackclient")
 
-prepend_path{"PATH"		,"/util/opt/anaconda/2.2.0/envs/python-openstackclient-2.1.0/bin",priority=100}
+prepend_path{"PATH"		,"/util/opt/anaconda/2.2/envs/python-openstackclient-2.1.0/bin",priority=100}
 pushenv("CONDA_DEFAULT_ENV"	,"python-openstackclient-2.1.0")
 prereq("python/2.7")
 family("anaconda")
