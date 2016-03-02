87e3a0531de7722bf956950990ae1d16897167fa
JingchaoZhang <zhang@unl.edu>
2016-03-02 16:04:46 -0600

    hdf5 version 1.8.15 with pgi/12 pgi/13 pgi/14 pgi/15 and openmpi/1.8

---
+++ MPI/pgi/15.10/openmpi/1.8.8/phdf5/1.8.lua
@@ -0,0 +1,22 @@
+local help_message = [[
+Parallel hdf5 1.8.15
+
+This module loads the hdf5, built with the PGI, OpenMPI
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: hdf5")
+whatis("Version: 1.8.15")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.hdfgroup.org/HDF5/release/obtain5.html#obtain")
+
+prepend_path("PATH",                "/util/opt/hdf5/1.8/openmpi/1.8/pgi/15/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/hdf5/1.8/openmpi/1.8/pgi/15/lib")
+prepend_path("MANPATH",             "/util/opt/hdf5/1.8/openmpi/1.8/pgi/15/share")
+prepend_path("INCLUDE",             "/util/opt/hdf5/1.8/openmpi/1.8/pgi/15/include")
+
+family("phdf5")
