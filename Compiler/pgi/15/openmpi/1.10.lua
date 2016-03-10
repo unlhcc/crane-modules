c13fa97c047da4da318bfdf7aa243e36f4e1f2d6
JingchaoZhang <zhang@unl.edu>
2016-03-10 12:49:51 -0600

    updated all openmpi modules to 1.10

---
+++ Compiler/pgi/15/openmpi/1.10.lua
@@ -0,0 +1,28 @@
+local help_message = [[
+OpenMPI Compilers 1.10.2
+
+This module loads the OpenMPI environment, built with the GNU 
+compilers.  By loading this module, the following commands
+will be automatically available for compiling MPI applications:
+
+mpif77		(F77 source)
+mpif90		(F90 source)
+mpicc		(C source)
+mpiCC/mpicxx	(C++ source)
+]]
+
+help(help_message,"\n")
+
+whatis("Name: OpenMPI")
+whatis("Version: 1.10.2")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.open-mpi.org")
+
+prepend_path("PATH",                "/util/opt/openmpi/1.10/pgi/15/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/openmpi/1.10/pgi/15/lib")
+prepend_path("MANPATH",             "/util/opt/openmpi/1.10/pgi/15/share/man")
+prepend_path("INCLUDE",             "/util/opt/openmpi/1.10/pgi/15/include")
+prepend_path("MODULEPATH",	    "/util/opt/modulefiles/MPI/pgi/15/openmpi/1.10")
+
+family("mpi")
