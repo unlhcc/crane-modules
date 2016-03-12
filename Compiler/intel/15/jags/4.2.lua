d933083d2b18a61bb09aec467a477e1a47c55d0d
Adam Caprez <acaprez2@unl.edu>
2016-03-11 18:03:58 -0600

    Add module file for JAGS 4.2

---
+++ Compiler/intel/15/jags/4.2.lua
@@ -0,0 +1,25 @@
+help(
+[[
+JAGS is Just Another Gibbs Sampler.  It is a program for analysis of Bayesian hierarchical models using Markov Chain Monte Carlo (MCMC) simulation not wholly unlike BUGS.  JAGS was written with three aims in mind:
+
+-To have a cross-platform engine for the BUGS language
+-To be extensible, allowing users to write their own functions, distributions and samplers.
+-To be a plaftorm for experimentation with ideas in Bayesian modelling
+
+JAGS is licensed under the GNU General Public License. You may freely modify and redistribute it under certain conditions (see the file COPYING for details).
+]]
+)
+
+whatis("Name: JAGS")
+whatis("Version: 4.2")
+whatis("Category: mathematics, statistics")
+whatis("Keywords: Mathematics, Statistics")
+whatis("URL: http://mcmc-jags.sourceforge.net")
+whatis("Description: JAGS is Just Another Gibbs Sampler.")
+
+
+prepend_path("PATH",              "/util/opt/jags/4.2/intel/15/bin")
+prepend_path("LD_LIBRARY_PATH",   "/util/opt/jags/4.2/intel/15/lib")
+prepend_path("MANPATH",   "/util/opt/jags/4.2/intel/15/share/man")
+
+prereq("intel-mkl/15")
