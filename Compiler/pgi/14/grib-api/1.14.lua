372754b9dd2a8c3d48d601e4bef820d9f6ec3b27
JingchaoZhang <zhang@unl.edu>
2016-03-14 13:28:03 -0500

    grib-api/1.14 with pgi/14 on Crane

---
+++ Compiler/pgi/14/grib-api/1.14.lua
@@ -0,0 +1,25 @@
+local help_message = [[
+GRIB-API 1.14.5
+
+This module loads the GRIB-API, built with the Intel 
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: GRIB-API")
+whatis("Version: 1.14.5")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: https://software.ecmwf.int/wiki/display/GRIB/Home")
+
+prepend_path("PATH",                "/util/opt/grib-api/1.14/pgi/14/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/grib-api/1.14/pgi/14/lib")
+prepend_path("MANPATH",             "/util/opt/grib-api/1.14/pgi/14/share/grib_api")
+prepend_path("INCLUDE",             "/util/opt/grib-api/1.14/pgi/14/include")
+setenv("GRIBAPI",                   "/util/opt/grib-api/1.14/pgi/14") 
+setenv("GRIBAPI_INCLUDE",           "/util/opt/grib-api/1.14/pgi/14/include")
+setenv("GRIBAPI_LIB",               "/util/opt/grib-api/1.14/pgi/14/lib")
+
+family("gribapi")
