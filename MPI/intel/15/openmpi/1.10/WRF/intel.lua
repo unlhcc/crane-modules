dfef8d625381663dc2a354b6517214558382c2b1
JingchaoZhang <zhang@unl.edu>
2016-03-15 14:15:34 -0500

    WRF update with intel/15,pgi/15 and openmpi/1.10 on Crane

--- MPI/intel/15/openmpi/1.10/WRF/intel.lua
+++ MPI/intel/15/openmpi/1.10/WRF/intel.lua
@@ -15,21 +15,22 @@ whatis("Keywords: System, Library")
 whatis("URL: http://www.wrf-model.org/index.php")
 
 -- A priority attribute in the netcdf module prepend_path function for
--- the PATH variable ensures that the netcdf 'ncdump' is first in
--- the path instead of the hdf4 one.
--- See RT #7945.
-load("compiler/intel/13","openmpi/1.6","NCL/6.1","NCO/4.4","udunits2/2.1","szip/2.1","hdf5/1.8","hdf4/4.2","netcdf/4.2")
+-- -- the PATH variable ensures that the netcdf 'ncdump' is first in
+-- -- the path instead of the hdf4 one.
+-- -- See RT #7945.
+load("compiler/intel/15","openmpi/1.10","NCL/6.3","NCO/4.4","udunits2/2.2","szip/2.1","hdf5/1.8","hdf4/4.2","netcdf/4.4")
 setenv("WRFIO_NCD_LARGE_FILE_SUPPORT", "1")
-setenv("JASPERINC", "/util/opt/jasper/1.900.1/intel/13/include")
-setenv("JASPERLIB", "/util/opt/jasper/1.900.1/intel/13/lib")
+setenv("JASPERINC", "/util/opt/jasper/1.900.1/intel/15/include")
+setenv("JASPERLIB", "/util/opt/jasper/1.900.1/intel/15/lib")
 
 --Cindy asked for this software RT# 4029
-prepend_path("PATH",                "/util/opt/cdo/1.6/intel/13/bin")
+prepend_path("PATH",                "/util/opt/cdo/1.7/intel/15/bin")
 
 --Cindy asked for this software RT# 4259
-prepend_path("PATH",                "/util/opt/wgrib/1.8/intel/13/bin")
+prepend_path("PATH",                "/util/opt/wgrib/1.8/intel/15/bin")
 
 --Cindy asked for this software RT# 6830
-prepend_path("PATH",                "/util/opt/ncview/2.1/intel/13/bin")
+prepend_path("PATH",                "/util/opt/ncview/2.1/intel/15/bin")
 
 family("WRF")
+
---
+++ MPI/pgi/15/openmpi/1.10/WRF/pgi.lua
@@ -0,0 +1,36 @@
+local help_message = [[
+WRF (Weather Research and Forecasting)
+
+This module loads the WRF environment, built with the PGI
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: WRF")
+whatis("Version: WRF")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.wrf-model.org/index.php")
+
+-- A priority attribute in the netcdf module prepend_path function for
+-- -- the PATH variable ensures that the netcdf 'ncdump' is first in
+-- -- the path instead of the hdf4 one.
+-- -- See RT #7945.
+load("compiler/pgi/15","openmpi/1.10","NCL/6.3","NCO/4.4","udunits2/2.2","szip/2.1","hdf5/1.8","hdf4/4.2","netcdf/4.4")
+setenv("WRFIO_NCD_LARGE_FILE_SUPPORT", "1")
+setenv("JASPERINC", "/util/opt/jasper/1.900.1/pgi/15/include")
+setenv("JASPERLIB", "/util/opt/jasper/1.900.1/pgi/15/lib")
+
+--Cindy asked for this software RT# 4029
+prepend_path("PATH",                "/util/opt/cdo/1.7/pgi/15/bin")
+
+--Cindy asked for this software RT# 4259
+prepend_path("PATH",                "/util/opt/wgrib/1.8/pgi/15/bin")
+
+--Cindy asked for this software RT# 6830
+prepend_path("PATH",                "/util/opt/ncview/2.1/pgi/15/bin")
+
+family("WRF")
+
