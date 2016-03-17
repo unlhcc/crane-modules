6f9a2c3c5ce256112d25973ceb92a8b14a927248
Adam Caprez <acaprez2@unl.edu>
2016-03-17 11:54:12 -0500

    Add module file for Intel 15 build of R

---
+++ Compiler/intel/15/R/3.2.lua
@@ -0,0 +1,23 @@
+local help_message = [[
+R 3.2
+
+This module loads the R environment, built with the Intel
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: R")
+whatis("Version: 3.2")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.r-project.org/")
+
+load("intel-mkl/15")
+prepend_path("PATH",                "/util/opt/R/3.2/intel/15/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/R/3.2/intel/15/lib64")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/R/3.2/intel/15/lib64/R/lib")
+prepend_path("MANPATH",             "/util/opt/R/3.2/intel/15/share/man/man1")
+
+family("R")
