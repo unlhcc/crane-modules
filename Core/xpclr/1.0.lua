f3f079bbeb7b7544b7cf6dad421bb9d0f26cafb7
JingchaoZhang <zhang@unl.edu>
2016-04-03 20:08:53 -0500

    XP-CLR 1.0 on Crane

---
+++ Core/xpclr/1.0.lua
@@ -0,0 +1,16 @@
+help (
+[[
+The XP-CLR package implements a composite likelihood method for detecting
+selective sweeps via the differentiation of two populations.
+]])
+
+whatis("Name: XP-CLR")
+whatis("Version: 1.0")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, High-throughput Sequencing")
+whatis("Description: detecting selective sweeps via the differentiation of two populations")
+whatis("URL: http://genetics.med.harvard.edu/reich/Reich_Lab/Software.html")
+
+prepend_path("PATH"             ,"/util/opt/xpclr/1.0/gcc/4.4/bin")
+family("xpclr")
+
