5ca78fce5773b8855b67bf4b2077d0445c71f0f9
Adam Caprez <acaprez2@unl.edu>
2016-04-12 11:09:13 -0500

    Add module file for csaw 1.4

---
+++ Core/csaw/1.4.lua
@@ -0,0 +1,15 @@
+help (
+[[
+This module loads CSAW
+Version 1.4
+]])
+
+whatis("Name: CSAW")
+whatis("Version: 1.4")
+whatis("Category: computational biology, sequencing")
+whatis("Keywords: Biology, Genomics, Sequencing")
+whatis("Description: Detection of differentially bound regions in ChIP-seq data with sliding windows, with methods for normalization and proper FDR control.")
+whatis("URL: https://bioconductor.org/packages/csaw")
+
+prepend_path{"PATH"		,"/util/opt/anaconda/2.2/envs/csaw-1.4.1/bin",priority=100}
+pushenv("CONDA_DEFAULT_ENV"	,"csaw-1.4.1")
