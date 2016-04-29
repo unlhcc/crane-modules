b9030fa83ac98cf991265f4fc93af393bc6cd453
JingchaoZhang <zhang@unl.edu>
2016-04-29 16:31:57 -0500

    parallel netcdf/4.4 with gcc/4.9 openmpi/1.10

---
+++ MPI/gcc/4.9/openmpi/1.10/pnetcdf/4.4.lua
@@ -0,0 +1,26 @@
+local help_message = [[
+netCDF 4.4
+
+This module loads the netCDF, built with the GCC 
+compilers and OpenMPI.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: netCDF")
+whatis("Version: 4.4")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.unidata.ucar.edu/downloads/netcdf/index.jsp")
+
+prepend_path("NETCDF",              "/util/opt/netcdf/4.4/openmpi/1.10/gcc/4.9")
+prepend_path("PATH",                "/util/opt/netcdf/4.4/openmpi/1.10/gcc/4.9/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/netcdf/4.4/openmpi/1.10/gcc/4.9/lib")
+prepend_path("INCLUDE",             "/util/opt/netcdf/4.4/openmpi/1.10/gcc/4.9/include")
+prepend_path("CPATH",               "/util/opt/netcdf/4.4/openmpi/1.10/gcc/4.9/include")
+prepend_path("LIBRARY_PATH",        "/util/opt/netcdf/4.4/openmpi/1.10/gcc/4.9/lib")
+prepend_path("PKG_CONFIG_PATH",     "/util/opt/netcdf/4.4/openmpi/1.10/gcc/4.9/lib/pkgconfig")
+
+family("pnetcdf")
+
