4276a97e32804ffffcc67f0836b5aaa3d75766b8
JingchaoZhang <zhang@unl.edu>
2016-04-29 14:37:20 -0500

    parallel hdf5 with gcc/4.9 openmpi/1.10

---
+++ MPI/gcc/4.9/openmpi/1.10/phdf5/1.10.lua
@@ -0,0 +1,24 @@
+local help_message = [[
+Parallel hdf5 1.8.15
+
+This module loads the hdf5, built with the GCC,OpenMPI
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: hdf5")
+whatis("Version: 1.8.15")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.hdfgroup.org/HDF5/release/obtain5.html#obtain")
+
+prepend_path("PATH",                "/util/opt/hdf5/1.8/openmpi/1.10/gcc/4.9/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/hdf5/1.8/openmpi/1.10/gcc/4.9/lib")
+prepend_path("MANPATH",             "/util/opt/hdf5/1.8/openmpi/1.10/gcc/4.9/share")
+prepend_path("INCLUDE",             "/util/opt/hdf5/1.8/openmpi/1.10/gcc/4.9/include")
+prepend_path("CPATH",               "/util/opt/hdf5/1.8/openmpi/1.10/gcc/4.9/include")
+prepend_path("LIBRARY_PATH",        "/util/opt/hdf5/1.8/openmpi/1.10/gcc/4.9/lib")
+
+family("phdf5")
