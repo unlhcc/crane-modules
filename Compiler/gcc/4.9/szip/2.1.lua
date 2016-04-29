af26fce5dee1b30fb00455fff8321300dffcb27a
JingchaoZhang <zhang@unl.edu>
2016-04-29 11:07:49 -0500

    zlib and szip with gcc/4.9

---
+++ Compiler/gcc/4.9/szip/2.1.lua
@@ -0,0 +1,24 @@
+local help_message = [[
+szip 2.1
+
+This module loads the szip, built with the GCC
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: szip")
+whatis("Version: 2.1")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.hdfgroup.org/doc_resource/SZIP")
+
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/szip/2.1/gcc/4.9/lib")
+prepend_path("INCLUDE",             "/util/opt/szip/2.1/gcc/4.9/include")
+
+family("szip")
+
+
+prepend_path("CPATH",             "/util/opt/szip/2.1/gcc/4.9/include")
+prepend_path("LIBRARY_PATH",     "/util/opt/szip/2.1/gcc/4.9/lib")
---
+++ Compiler/gcc/4.9/zlib/1.2.lua
@@ -0,0 +1,26 @@
+local help_message = [[
+zlib 1.2
+
+This module loads the zlib, built with the GCC
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: zlib")
+whatis("Version: 1.2")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.zlib.net/")
+
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/zlib/1.2/gcc/4.9/lib")
+prepend_path("MANPATH",             "/util/opt/zlib/1.2/gcc/4.9/share/man")
+prepend_path("INCLUDE",             "/util/opt/zlib/1.2/gcc/4.9/include")
+
+family("zlib")
+
+
+prepend_path("CPATH",             "/util/opt/zlib/1.2/gcc/4.9/include")
+prepend_path("LIBRARY_PATH",     "/util/opt/zlib/1.2/gcc/4.9/lib")
+prepend_path("PKG_CONFIG_PATH",        "/util/opt/zlib/1.2/gcc/4.9/lib/pkgconfig")
