5ea427df05a35bbb72affabf2110ac84fac378e5
JingchaoZhang <zhang@unl.edu>
2016-05-26 12:18:46 -0500

    art on Crane

---
+++ Core/art/2.6.lua
@@ -0,0 +1,17 @@
+help (
+[[
+ART is a set of simulation tools to generate synthetic next-generation sequencing reads. ART simulates sequencing reads by mimicking real sequencing process with empirical error models or quality profiles summarized from large recalibrated sequencing data. 
+
+Version 2.6.0
+
+]])
+
+whatis("Name: ART")
+whatis("Version: 2.6.0")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Alignment, Sequencing")
+whatis("URL: http://www.niehs.nih.gov/research/resources/software/biostatistics/art/")
+whatis("Description: ART is a set of simulation tools to generate synthetic next-generation sequencing reads.")
+
+prepend_path("PATH","/util/opt/art/2.6/gcc/4.4/bin")
+family("art")
