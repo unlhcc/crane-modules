0edba14ec32820120dc03fac4808ad5df9ec207f
Adam Caprez <acaprez2@unl.edu>
2016-06-06 12:59:43 -0500

    Add module file for gcc 5.4.

---
+++ Core/compiler/gcc/5.4.lua
@@ -0,0 +1,43 @@
+local help_message = [[
+GCC Compilers 5.4
+
+This module loads GCC Compiler variables.
+The command directory is added to PATH.
+The library directory is added to LD_LIBRARY_PATH.
+The include directory is added to INCLUDE.
+The man     directory is added to MANPATH.
+]]
+
+help(help_message,"\n")
+
+whatis("Name: GCC Compilers")
+whatis("Version: 5.4")
+whatis("Category: compiler")
+whatis("Keywords: System, compiler")
+whatis("URL: http://gcc.gnu.org")
+
+prepend_path("PATH",                "/util/comp/gcc/5.4/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/comp/gcc/5.4/lib")
+prepend_path("LD_LIBRARY_PATH",     "/util/comp/gcc/5.4/lib64")
+prepend_path("LIBRARY_PATH",     "/util/comp/gcc/5.4/lib")
+prepend_path("LIBRARY_PATH",     "/util/comp/gcc/5.4/lib64")
+prepend_path("MANPATH",             "/util/comp/gcc/5.4/man")
+prepend_path("INCLUDE",             "/util/comp/gcc/5.4/include")
+prepend_path("PKG_CONFIG_PATH",     "/util/comp/gcc/5.4/lib/pkgconfig")
+prepend_path("PKG_CONFIG_PATH",     "/util/comp/gcc/5.4/lib64/pkgconfig")
+prepend_path("MODULEPATH",          "/util/opt/modulefiles/Compiler/gcc/5.4")
+setenv(      "GCC_LIB",             "/util/comp/gcc/5.4/lib64")
+
+setenv("CC",	"gcc")
+setenv("FC",	"gfortran")
+setenv("F90",	"gfortran")
+setenv("F77",	"gfortran")
+setenv("CXX",	"g++")
+
+setenv("CFLAGS",	"-march=corei7-avx")
+setenv("FFLAGS",	"-march=corei7-avx")
+setenv("CXXFLAGS",        "-march=corei7-avx")
+setenv("F90FLAGS",        "-march=corei7-avx")
+setenv("FCFLAGS",        "-march=corei7-avx")
+
+family("compiler")
