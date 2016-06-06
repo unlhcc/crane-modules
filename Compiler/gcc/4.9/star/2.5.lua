d9fc16a96949d6aeb27f52bb045c7829a85706cb
Adam Caprez <acaprez2@unl.edu>
2016-06-06 11:15:46 -0500

    Add module file for star 2.5.

---
+++ Compiler/gcc/4.9/star/2.5.lua
@@ -0,0 +1,17 @@
+help(
+[[
+The star module file defines the following environment variables:
+PATH for the location of the star binaries.
+
+Version 2.5
+]]
+)
+
+whatis("Name: STAR")
+whatis("Version: 2.5.2a")
+whatis("Category: biology, genomics")
+whatis("Keywords: Biology, Genomics")
+whatis("URL: https://github.com/alexdobin/STAR")
+whatis("Description: Spliced Transcripts Alignment to a Reference")
+
+prepend_path("PATH",              "/util/opt/star/2.5/gcc/4.9/bin")
