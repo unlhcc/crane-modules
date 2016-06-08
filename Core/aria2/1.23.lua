5331bc825492829ea25109248e4e9914a61d9ab8
Adam Caprez <acaprez2@unl.edu>
2016-06-08 13:22:23 -0500

    Add module file for aria2 1.23.

---
+++ Core/aria2/1.23.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads aria2.
+PATH has been prepended to include the aria2 program.
+Version 1.23.0
+]])
+
+whatis("Name: aria2")
+whatis("Version: 1.23.0")
+whatis("Category: tool, utility")
+whatis("Keywords: Tools, Utilities")
+whatis("Description: aria2 is a lightweight multi-protocol & multi-source command-line download utility. ")
+whatis("URL: https://aria2.github.io")
+
+prepend_path{"PATH"		,"/util/opt/anaconda/2.2/envs/aria2-1.23.0/bin",priority=100}
+pushenv("CONDA_DEFAULT_ENV"	,"aria2-1.23.0")
