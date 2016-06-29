66f77e54dc54f3d2ffdad675ebdb682520855f0c
Adam Caprez <acaprez2@unl.edu>
2016-06-29 14:00:50 -0500

    Add module file for SNP pipeline 0.6.

---
+++ Core/snp-pipeline/0.6.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads SNP Pipeline
+PATH has been prepended to include the SNP Pipeline program.
+Version 0.6.
+]])
+
+whatis("Name: SNP Pipeline")
+whatis("Version: 0.6")
+whatis("Category: computational biology, sequencing")
+whatis("Keywords: Biology, Genomics, Sequencing")
+whatis("Description: SNP Pipeline is a pipeline for the production of SNP matrices from sequence data used in the phylogenetic analysis of pathogenic organisms sequenced from samples of interest to food safety.")
+whatis("URL: http://snp-pipeline.readthedocs.io")
+
+prepend_path{"PATH"		,"/util/opt/anaconda/2.2/envs/snp-pipeline-0.6.1/bin",priority=100}
+pushenv("CONDA_DEFAULT_ENV"	,"snp-pipeline-0.6.1")
