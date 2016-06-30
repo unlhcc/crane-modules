a4bb6991ef403107b025508853b5cc58864653ff
JingchaoZhang <zhang@unl.edu>
2016-06-30 16:21:27 -0500

    udunits2 and hdf4 with compiler/intel/16

---
+++ Compiler/intel/16/hdf4/4.2.lua
@@ -0,0 +1,26 @@
+local help_message = [[
+hdf4 4.2
+
+This module loads the hdf4, built with the Intel 
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: hdf4")
+whatis("Version: 4.2")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.hdfgroup.org/release4/obtain.html#obtain")
+
+prepend_path("PATH",                "/util/opt/hdf4/4.2/intel/16/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/hdf4/4.2/intel/16/lib")
+prepend_path("MANPATH",             "/util/opt/hdf4/4.2/intel/16/share/man")
+prepend_path("INCLUDE",             "/util/opt/hdf4/4.2/intel/16/include")
+
+load("szip/2.1","zlib/1.2")
+family("hdf4")
+
+prepend_path("CPATH",             "/util/opt/hdf4/4.2/intel/16/include")
+prepend_path("LIBRARY_PATH",     "/util/opt/hdf4/4.2/intel/16/lib")
---
+++ Compiler/intel/16/udunits2/2.2.lua
@@ -0,0 +1,25 @@
+local help_message = [[
+udunits 2.2
+
+This module loads the udunits2 package, built with the Intel
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: udunits2")
+whatis("Version: 2.2")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.unidata.ucar.edu/software/udunits")
+
+prepend_path("PATH",		    "/util/opt/udunits/2.2/intel/16/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/udunits/2.2/intel/16/lib")
+prepend_path("MANPATH",             "/util/opt/udunits/2.2/intel/16/share")
+prepend_path("INCLUDE",             "/util/opt/udunits/2.2/intel/16/include")
+
+family("udunits2")
+
+prepend_path("CPATH",             "/util/opt/udunits/2.2/intel/16/include")
+prepend_path("LIBRARY_PATH",     "/util/opt/udunits/2.2/intel/16/lib")
