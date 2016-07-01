81df3990e04c6920e5bf5f5e480ad0a7d78cfe5e
JingchaoZhang <zhang@unl.edu>
2016-07-01 16:27:56 -0500

    openmpi/1.10 with compiler/intel/16

---
+++ Compiler/intel/16/openmpi/1.10.lua
@@ -0,0 +1,32 @@
+local help_message = [[
+OpenMPI Compilers 1.10.2
+
+This module loads the OpenMPI environment, built with the GNU 
+compilers.  By loading this module, the following commands
+will be automatically available for compiling MPI applications:
+
+mpif77		(F77 source)
+mpif90		(F90 source)
+mpicc		(C source)
+mpiCC/mpicxx	(C++ source)
+]]
+
+help(help_message,"\n")
+
+whatis("Name: OpenMPI")
+whatis("Version: 1.10.2")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.open-mpi.org")
+
+prepend_path("PATH",                "/util/opt/openmpi/1.10/intel/16/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/openmpi/1.10/intel/16/lib")
+prepend_path("MANPATH",             "/util/opt/openmpi/1.10/intel/16/share/man")
+prepend_path("INCLUDE",             "/util/opt/openmpi/1.10/intel/16/include")
+prepend_path("MODULEPATH",	    "/util/opt/modulefiles/MPI/intel/16/openmpi/1.10")
+
+family("mpi")
+
+prepend_path("CPATH",             "/util/opt/openmpi/1.10/intel/16/include")
+prepend_path("LIBRARY_PATH",     "/util/opt/openmpi/1.10/intel/16/lib")
+prepend_path("PKG_CONFIG_PATH",        "/util/opt/openmpi/1.10/intel/16/lib/pkgconfig")
