cee55f7da725e8e2ed1037b1b9efe6c6d25ca81e
Tom Harvill <tharvill@login.crane.hcc.unl.edu>
2016-08-08 15:49:43 -0500

    matlab r2016a

---
+++ Core/matlab/r2016.lua
@@ -0,0 +1,14 @@
+local help_message = [[
+MATLAB r2016a
+]]
+
+help(help_message,"\n")
+
+whatis("Name: MATLAB r2016a")
+whatis("Keywords: Application, Engineering, Math")
+
+local WORK = os.getenv("WORK")
+--setenv("MATLAB_PREFDIR", WORK .. "/.matlab")
+setenv("MATLAB_PREFDIR", "/tmp")
+
+prepend_path("PATH","/util/opt/matlab/r2016a/bin")
