3b69036163141a5a1ab80c5e2a2c6195dbbcee95
JingchaoZhang <zhang@unl.edu>
2016-08-08 16:53:18 -0500

    modified caffe module

--- Core/caffe/1.0.lua
+++ Core/caffe/1.0.lua
@@ -12,7 +12,10 @@ whatis("Keywords: deep learning")
 whatis("Description: It is developed by the Berkeley Vision and Learning Center (BVLC) and by community contributors. Yangqing Jia created the project during his PhD at UC Berkeley. Caffe is released under the BSD 2-Clause license.")
 whatis("URL: http://caffe.berkeleyvision.org/")
 
+load("anaconda/2.7","gflags/2.1","glog/0.3","boost/1.55.0","opencv/3.1","lmdb/0.9","hdf5/1.8")
+
 prepend_path("PATH",			"/util/opt/caffe/1.0/gcc/4.4/bin")
 prepend_path("LD_LIBRARY_PATH",		"/util/opt/caffe/1.0/gcc/4.4/lib")
 prepend_path("INCLUDE",			"/util/opt/caffe/1.0/gcc/4.4/include")
-prepend_path("CPATH",                 "/util/opt/caffe/1.0/gcc/4.4/include")
+prepend_path("CPATH",                   "/util/opt/caffe/1.0/gcc/4.4/include")
+prepend_path("PYTHONPATH",		"/util/opt/caffe/1.0/gcc/4.4/python/")
