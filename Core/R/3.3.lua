d19a64f8e391d223fe322fe543ed75fd08966802
Adam Caprez <acaprez2@unl.edu>
2016-09-06 10:17:10 -0500

    Add moduel file for R 3.3 built with GCC 4.4.

---
+++ Core/R/3.3.lua
@@ -0,0 +1,25 @@
+local help_message = [[
+R 3.3
+
+This module loads the R environment, built with the GCC
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: R")
+whatis("Version: 3.3")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.r-project.org/")
+
+load("intel-mkl/15","xzutils/5.2")
+prepend_path("PATH",                "/util/opt/R/3.3/gcc/4.4/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/R/3.3/gcc/4.4/lib64")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/R/3.3/gcc/4.4/lib64/R/lib")
+prepend_path("LIBRARY_PATH",        "/util/opt/R/3.3/gcc/4.4/lib64/R/lib")
+prepend_path("MANPATH",             "/util/opt/R/3.3/gcc/4.4/share/man/man1")
+prepend_path("PKG_CONFIG_PATH",     "/util/opt/R/3.3/gcc/4.4/lib64/pkgconfig")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/pcre/8.39/gcc//4.4/lib")
+family("R")
