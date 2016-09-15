f95bccf53077096cde4337f0e2b37fb70b963d84
Adam Caprez <acaprez2@unl.edu>
2016-09-15 13:24:05 -0500

    Add module file for CCAT 3.0.

---
+++ Core/ccat/3.0.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads ccat.
+PATH has been prepended to include the ccat program.
+Version 3.0
+]])
+
+whatis("Name: ccat")
+whatis("Version: 3.0")
+whatis("Category: computational biology, sequencing")
+whatis("Keywords: Biology, Genomics, Sequencing")
+whatis("Description: A software package for the analysis of ChIP-seq data with negative control.")
+whatis("URL: http://cmb.gis.a-star.edu.sg/ChIPSeq/paperCCAT.htm")
+
+prepend_path{"PATH"		,"/util/opt/anaconda/2.2/envs/ccat-3.0/bin"}
+pushenv("CONDA_DEFAULT_ENV"	,"ccat-3.0")
