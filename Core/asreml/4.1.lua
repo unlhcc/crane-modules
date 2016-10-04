50f84dbb9111aad771212d7270345667a108031d
Adam Caprez <acaprez2@unl.edu>
2016-10-04 10:48:36 -0500

    Add module file for ASreml 4.1.

---
+++ Core/asreml/4.1.lua
@@ -0,0 +1,15 @@
+help (
+[[
+This module loads ASReml.
+Version 4.1.
+]])
+
+whatis("Name: Asreml")
+whatis("Version: 4.1")
+whatis("Category: data analysis, statistics")
+whatis("Keywords: Data Analysis, Statistics")
+whatis("Description: ASReml")
+whatis("URL: http://www.vsni.co.uk/software/asreml")
+
+prepend_path("PATH"		,"/util/opt/asreml/4.1/bin")
+setenv("ASREML_LICENSE_FILE"	,"/util/opt/asreml/4.1/bin/asreml.lic")
