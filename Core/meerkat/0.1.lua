c8980a1ece7c1a620f475a4e47d2c88dd2844629
JingchaoZhang <zhang@unl.edu>
2016-10-12 11:15:00 -0500

    Meerkat 0.189 on Crane

---
+++ Core/meerkat/0.1.lua
@@ -0,0 +1,20 @@
+help (
+[[
+This module loads Meerkat built with GCC.
+Meerkat is designed to identify structural variations (SVs) from paired end high throughput sequencing
+data
+
+Version 0.189
+]])
+
+whatis("Name: meerkat")
+whatis("Version: 0.189")
+whatis("Category: computational biology, genomics")
+whatis("Keywords:  Biology, Genomics, Alignment, Sequencing")
+whatis("Description: Meerkat is designed to identify structural variations ")
+whatis("URL: http://compbio.med.harvard.edu/Meerkat/")
+
+load("primer3/2.3","blat/35x1","blast/2.4","bwa/0.6","samtools/0.1","intel-mkl/15","xzutils/5.2","R/3.3","bioperl/1.6")
+prepend_path("PATH"       ,"/util/opt/meerkat/0.1/gcc/4.4/bin/")
+prepend_path("PATH"       ,"/util/opt/meerkat/0.1/gcc/4.4/scripts/")
+
