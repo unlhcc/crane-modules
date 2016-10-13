9aace6fb54aaa2a9aac64ea5e97b7aea3864597d
Adam Caprez <acaprez2@unl.edu>
2016-10-13 16:36:36 -0500

    Add module file for SAGA 2.1.

---
+++ Compiler/gcc/4.9/saga/2.1.lua
@@ -0,0 +1,22 @@
+help(
+[[
+SAGA: System for Automated Geoscientific Analyses
+
+Version 2.1
+]]
+)
+
+whatis("Name: Saga")
+whatis("Version: 2.1")
+whatis("Category: GIS")
+whatis("Keywords: GIS ")
+whatis("URL: http://www.saga-gis.org/en/index.html")
+whatis("Description: SAGA: System for Automated Geoscientific Analyses.")
+
+
+prepend_path("PATH",              "/util/opt/saga/2.1/gcc/4.9/bin")
+prepend_path("LD_LIBRARY_PATH",	  "/util/opt/saga/2.1/gcc/4.9/lib")
+prepend_path("LD_LIBRARY_PATH",   "/util/opt/gtk+/3.0/gcc/4.9/lib")
+prepend_path("LD_LIBRARY_PATH",   "/util/opt/wxwidgets/3.0/gcc/4.9/lib")
+prepend_path("LD_LIBRARY_PATH",   "/util/opt/cairo/1.14/gcc/4.9/lib")
+prepend_path("LD_LIBRARY_PATH",   "/util/opt/glib/2.50/gcc/4.9/lib")
