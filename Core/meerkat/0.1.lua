b494eedcc15781fa6da44647cf02168f4298571f
JingchaoZhang <zhang@unl.edu>
2016-10-21 14:16:03 -0500

    added LD_LIBRARY_PATH to meerkat

--- Core/meerkat/0.1.lua
+++ Core/meerkat/0.1.lua
@@ -17,4 +17,4 @@ whatis("URL: http://compbio.med.harvard.edu/Meerkat/")
 load("primer3/2.3","blat/35x1","blast/2.4","bwa/0.6","samtools/0.1","intel-mkl/15","xzutils/5.2","R/3.3","bioperl/1.6")
 prepend_path("PATH"       ,"/util/opt/meerkat/0.1/gcc/4.4/bin/")
 prepend_path("PATH"       ,"/util/opt/meerkat/0.1/gcc/4.4/scripts/")
-
+prepend_path("LD_LIBRARY_PATH"	,"/util/src/meerkat/Meerkat/src/mybamtools/lib")
