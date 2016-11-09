b79e55784e7f172d1e6b35e98855108d582f49b6
Qidong Jia <qidong@login.crane.hcc.unl.edu>
2016-11-09 16:30:06 -0600

    Corrected hisat2

--- Core/hisat2/2.0.lua
+++ Core/hisat2/2.0.lua
@@ -14,6 +14,4 @@ whatis("URL: https://ccb.jhu.edu/software/hisat2/index.shtml")
 whatis("Description: HISAT2 is a fast and sensitive alignment program for mapping next-generation sequencing reads (both DNA and RNA) to a population of human genomes (as well as to a single reference genome).")
 prepend_path("PATH","/util/opt/BCRF/hisat2/2.0/gcc/4.4/")
 prepend_path("PATH","/util/opt/BCRF/hisat2/2.0/gcc/4.4/scripts/")
-prepend_path("PATH","/util/opt/BCRF/hisat2/2.0/gcc/4.4/example/")
-prepend_path("PATH","/util/opt/BCRF/hisat2/2.0/gcc/4.4/doc/")
 
