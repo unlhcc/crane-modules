319d4831396055ecacc8bc6b501df157b247ae91
Adam Caprez <acaprez2@unl.edu>
2016-12-01 10:33:05 -0600

    Add module file for autoconf 2.69.

---
+++ Core/autoconf/2.69.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads autoconf.
+
+Version 2.69.
+]])
+
+whatis("Name: autoconf")
+whatis("Version: 2.69")
+whatis("Category: Utilities")
+whatis("Keywords:  Utilities")
+whatis("Description: Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages.")
+whatis("URL: https://www.gnu.org/software/autoconf/autoconf.html")
+
+prepend_path("PATH"       ,"/util/opt/autoconf/2.69/gcc/4.4/bin")
+prepend_path("MANPATH"	  ,"/util/opt/autoconf/2.69/gcc/4.4/share/man")
