aed1b3d0337d67b96823d4e744d11530838d7826
Adam Caprez <acaprez2@unl.edu>
2016-12-01 10:39:11 -0600

    Add module file for automake 1.15.

---
+++ Core/automake/1.15.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads automake.
+
+Version 1.15.
+]])
+
+whatis("Name: automake")
+whatis("Version: 1.15")
+whatis("Category: Utilities")
+whatis("Keywords:  Utilities")
+whatis("Description: Automake is a tool for automatically generating Makefile.in files compliant with the GNU Coding Standards.")
+whatis("URL: https://www.gnu.org/software/automake/automake.html")
+
+prepend_path("PATH"       ,"/util/opt/automake/1.15/gcc/4.4/bin")
+prepend_path("MANPATH"	  ,"/util/opt/automake/1.15/gcc/4.4/share/man")
