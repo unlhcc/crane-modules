7c8463baa9d654bbc6883d007d625e1c2739b484
Adam Caprez <acaprez2@unl.edu>
2016-12-01 10:25:32 -0600

    Add module file for m4 1.4.

---
+++ Core/m4/1.4.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads m4.
+
+Version 1.4.
+]])
+
+whatis("Name: m4")
+whatis("Version: 1.4")
+whatis("Category: Utilities")
+whatis("Keywords:  Utilities")
+whatis("Description: GNU M4 is an implementation of the traditional Unix macro processor.")
+whatis("URL: https://www.gnu.org/software/m4/m4.html")
+
+prepend_path("PATH"       ,"/util/opt/m4/1.4/gcc/4.4/bin")
+prepend_path("MANPATH"	  ,"/util/opt/m4/1.4/gcc/4.4/share/man")
