b8f29c693cf7d8bdaa9e17659468249b95064183
Adam Caprez <acaprez2@unl.edu>
2017-01-11 15:41:50 -0600

    Add module file for SRAtoolkit 2.8.

---
+++ Core/SRAtoolkit/2.8.lua
@@ -0,0 +1,13 @@
+help (
+[[
+This module loads the SRA Toolkit.
+Version 2.8.
+]])
+
+whatis("Name: SRA Toolkit")
+whatis("Version: 2.8")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, High-throughput Sequencing")
+whatis("Description: SRA Toolkit")
+
+prepend_path("PATH"		,"/util/opt/sratools/2.8/bin")
