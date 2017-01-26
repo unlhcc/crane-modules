24f2208a5905728966af0cf28b97248d4fad326e
JingchaoZhang <zhang@unl.edu>
2017-01-26 12:42:25 -0600

    added psm2-compat to intel compiled openmpi 1.6-1.10

--- Compiler/intel/16/openmpi/1.10.lua
+++ Compiler/intel/16/openmpi/1.10.lua
@@ -21,6 +21,7 @@ whatis("URL: http://www.open-mpi.org")
 
 prepend_path("PATH",                "/util/opt/openmpi/1.10/intel/16/bin")
 prepend_path("LD_LIBRARY_PATH",     "/util/opt/openmpi/1.10/intel/16/lib")
+prepend_path("LD_LIBRARY_PATH",     "/usr/lib64/psm2-compat")
 prepend_path("MANPATH",             "/util/opt/openmpi/1.10/intel/16/share/man")
 prepend_path("INCLUDE",             "/util/opt/openmpi/1.10/intel/16/include")
 prepend_path("MODULEPATH",	    "/util/opt/modulefiles/MPI/intel/16/openmpi/1.10")
