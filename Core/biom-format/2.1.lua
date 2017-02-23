cf0cfd547945ae96a499c1bae8dc14e2a6834bf4
Adam Caprez <acaprez2@unl.edu>
2017-02-23 13:56:20 -0600

    Add module file for biom-format 2.1.

---
+++ Core/biom-format/2.1.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads biom-format
+Version 2.1
+
+]])
+
+whatis("Name: GEM")
+whatis("Version: 2.1")
+whatis("Category: computational biology, sequencing")
+whatis("Keywords: Biology, Genomics, Sequencing")
+whatis("Description: The BIOM file format (canonically pronounced biome) is designed to be a general-use format for representing biological sample by observation contingency tables. BIOM is a recognized standard for the Earth Microbiome Project and is a Genomics Standards Consortium supported project.")
+whatis("URL: http://biom-format.org")
+
+prepend_path{"PATH"		,"/util/opt/anaconda/2.2/envs/biom-format-2.1.5/bin",priority=100}
+pushenv("CONDA_DEFAULT_ENV"	,"biom-format-2.1")
