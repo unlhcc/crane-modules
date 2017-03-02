f9768bd215ca36996ad6a1bc16685aa327326ba6
Qidong Jia <qidong@login.crane.hcc.unl.edu>
2017-03-02 12:42:30 -0600

    MEME added

---
+++ Core/MEME/4.11.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads the MEME suite.
+
+Version 4.11.3
+]]
+)
+
+whatis("Name: MEME")
+whatis("Version: 4.11.3")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, RNAseq, Transcriptome profiling, Alignment")
+whatis("URL: http://meme-suite.org/")
+whatis("Description: The MEME suite is a collection of tools for the discovery and analysis of sequence motifs. It is hosted at http://meme-suite.org/.")
+
+prepend_path("PATH","/util/opt/BCRF/MEME/4.11/gcc/4.4.7/bin")
