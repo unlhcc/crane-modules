945238c773bee8b7900f65581a464ac9efb7b564
JingchaoZhang <zhang@unl.edu>
2017-03-03 10:38:52 -0600

    hapflk 1.2.1 on Crane

---
+++ Core/hapflk/1.2.lua
@@ -0,0 +1,16 @@
+help (
+[[
+hapflk is a software implementing the hapFLK [1] and FLK [2] tests for the detection of selection signatures based on multiple population genotyping data.
+
+Version 1.2.1
+]])
+
+whatis("Name: hapflk")
+whatis("Version: 1.2.1")
+whatis("Category: package")
+whatis("Keywords: bioinformatics")
+whatis("Description: hapflk is a software implementing the hapFLK [1] and FLK [2] tests for the detection of selection signatures based on multiple population genotyping data.")
+whatis("URL: https://forge-dga.jouy.inra.fr/projects/hapflk")
+
+prepend_path("PATH",			"/util/opt/hapflk/1.2/gcc/4.4/bin")
+prepend_path("PYTHONPATH",		"/util/opt/hapflk/1.2/gcc/4.4/lib64/python2.6/site-packages")
