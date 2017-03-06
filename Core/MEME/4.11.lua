708c2e957938449cbaaf85a4c4f308ad42d62745
Adam Caprez <acaprez2@unl.edu>
2017-03-06 11:06:12 -0600

    Use symlink path.

--- Core/MEME/4.11.lua
+++ Core/MEME/4.11.lua
@@ -13,4 +13,4 @@ whatis("Keywords: Biology, Genomics, RNAseq, Transcriptome profiling, Alignment"
 whatis("URL: http://meme-suite.org/")
 whatis("Description: The MEME suite is a collection of tools for the discovery and analysis of sequence motifs. It is hosted at http://meme-suite.org/.")
 
-prepend_path("PATH","/util/opt/BCRF/MEME/4.11/gcc/4.4.7/bin")
+prepend_path("PATH","/util/opt/BCRF/MEME/4.11/gcc/4.4/bin")
