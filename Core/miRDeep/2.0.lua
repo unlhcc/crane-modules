f4a2aa1864bb7a3348881d4014014729ceacdd9b
JingchaoZhang <zhang@unl.edu>
2017-03-21 16:23:28 -0500

    miRDeep 2.0.0 on Crane

---
+++ Core/miRDeep/2.0.lua
@@ -0,0 +1,20 @@
+help(
+[[
+miRDeep2 is a completely overhauled tool which discovers microRNA genes by analyzing sequenced RNAs. The tool reports known and hundreds of novel microRNAs with high accuracy in seven species representing the major animal clades. The low consumption of time and memory combined with user-friendly interactive graphic output makes miRDeep2 accessible for straightforward application in current reasearch.
+
+Version 2.0.0
+]]
+)
+
+whatis("Name: miRDeep2")
+whatis("Version: 2.0.0")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Quality Control, Utility, Sequencing, Genotyping, Resequencing, SNP")
+whatis("URL: https://www.mdc-berlin.de/8551903/en/")
+whatis("Description: miRDeep2 is a completely overhauled tool which discovers microRNA genes by analyzing sequenced RNAs.")
+
+
+prepend_path("PATH",              "/util/opt/miRDeep/2.0/gcc/4.4/bin")
+prepend_path("LD_LIBRARY_PATH",   "/util/opt/miRDeep/2.0/gcc/4.4/lib")
+prepend_path("INCLUDE",           "/util/opt/miRDeep/2.0/gcc/4.4/include")
+prepend_path("MANPATH",		  "/util/opt/miRDeep/2.0/gcc/4.4/share/man")
