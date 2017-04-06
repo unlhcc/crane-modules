6afc182657f008b121db58a382a465b2a3d9e817
Adam Caprez <acaprez2@unl.edu>
2017-04-06 17:24:27 -0500

    Add module file for beagle 4.1.

---
+++ Core/beagle/4.1.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads Beagle.
+Version 4.1
+
+]])
+
+whatis("Name: Beagle")
+whatis("Version: 4.1_21Jan17")
+whatis("Category: Biology, Genotyping")
+whatis("Keywords: Biology, Genotyping")
+whatis("URL: https://faculty.washington.edu/browning/beagle/beagle.html")
+whatis("Description: Beagle is a software package that performs genotype calling, genotype phasing, imputation of ungenotyped markers, and identity-by-descent segment detection.")
+
+pushenv("CONDA_DEFAULT_ENV","beagle-4.1_21Jan17.6cc.jar")
+prepend_path{"PATH","/util/opt/anaconda/2.2/envs/beagle-4.1_21Jan17.6cc.jar/bin",priority=100}
