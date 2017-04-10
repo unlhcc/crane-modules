f7fb284b98ba4a20316817ed161decffafa16bc2
Adam Caprez <acaprez2@unl.edu>
2017-04-10 11:34:42 -0500

    Add module for mxnet 0.9.3a.

---
+++ Core/mxnet/0.9.3.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads MXNet.
+Version 0.9.3a
+
+]])
+
+whatis("Name: MXNet")
+whatis("Version: 0.9.3a")
+whatis("Category: AI, neural networks, machine learning")
+whatis("Keywords: AI, neural networks, machine learning")
+whatis("URL: http://mxnet.io")
+whatis("Description: MXNet is an open-source deep learning framework that allows you to define, train, and deploy deep neural networks on a wide array of devices, from cloud infrastructure to mobile devices.")
+
+pushenv("CONDA_DEFAULT_ENV","mxnet-0.9.3a")
+prepend_path{"PATH","/util/opt/anaconda/2.2/envs/mxnet-0.9.3a/bin",priority=100}
