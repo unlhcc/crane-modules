b30ed694f4c3254ad0413b29aa4b3cfcf7c8e107
Adam Caprez <acaprez2@unl.edu>
2017-04-26 12:24:28 -0500

    Add module file for Maven 3.5.

---
+++ Core/maven/3.5.lua
@@ -0,0 +1,15 @@
+help (
+[[
+This module loads Apache Maven.
+
+Version 3.5.
+]])
+
+whatis("Name: maven")
+whatis("Version: 3.5")
+whatis("Category: system, build tool")
+whatis("Keywords:  system, utility, build tool")
+whatis("Description: Apache Maven is a software project management and comprehension tool.")
+whatis("URL: http://maven.apache.org/index.html")
+
+prepend_path("PATH"       ,"/util/opt/maven/3.5/bin")
