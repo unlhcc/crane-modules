0feebccbb2b075bf83e1b0b410e378b57ab8b848
JingchaoZhang <zhang@unl.edu>
2017-05-16 11:06:10 -0500

    openmpi 2.1.1 on Crane

---
+++ Compiler/intel/12/openmpi/2.1.lua
@@ -0,0 +1,32 @@
+local help_message = [[
+OpenMPI Compilers 2.1
+
+This module loads the OpenMPI environment, built with the Intel
+compilers.  By loading this module, the following commands
+will be automatically available for compiling MPI applications:
+
+mpif77		(F77 source)
+mpif90		(F90 source)
+mpicc		(C source)
+mpiCC/mpicxx	(C++ source)
+]]
+
+help(help_message,"\n")
+
+whatis("Name: OpenMPI")
+whatis("Version: 2.1")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.open-mpi.org")
+
+prepend_path("PATH",                "/util/opt/openmpi/2.1/intel/12/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/openmpi/2.1/intel/12/lib")
+prepend_path("MANPATH",             "/util/opt/openmpi/2.1/intel/12/share/man")
+prepend_path("INCLUDE",             "/util/opt/openmpi/2.1/intel/12/include")
+prepend_path("MODULEPATH",          "/util/opt/modulefiles/MPI/intel/12/openmpi/2.1")
+
+family("mpi")
+
+prepend_path("CPATH",             "/util/opt/openmpi/2.1/intel/12/include")
+prepend_path("LIBRARY_PATH",     "/util/opt/openmpi/2.1/intel/12/lib")
+prepend_path("PKG_CONFIG_PATH",        "/util/opt/openmpi/2.1/intel/12/lib/pkgconfig")
