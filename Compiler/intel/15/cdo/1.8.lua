258b59ff49889106cae892b977d7478016983303
JingchaoZhang <zhang@unl.edu>
2017-05-18 15:18:45 -0500

    cdo version 1.8.1 with multiple intel and pgi compilers

---
+++ Compiler/intel/15/cdo/1.8.lua
@@ -0,0 +1,20 @@
+local help_message = [[
+cdo 1.8.1
+
+This module loads the CDO package, built with the Intel
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: cdo")
+whatis("Version: 1.8.1")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: https://code.zmaw.de/projects/cdo")
+
+prepend_path("PATH",                "/util/opt/cdo/1.8/intel/15/bin")
+
+family("cdo")
+
---
+++ Compiler/pgi/13/cdo/1.8.lua
@@ -0,0 +1,20 @@
+local help_message = [[
+cdo 1.8
+
+This module loads the CDO package, built with the PGI
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: cdo")
+whatis("Version: 1.8")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: https://code.zmaw.de/projects/cdo")
+
+prepend_path("PATH",                "/util/opt/cdo/1.8/pgi/13/bin")
+
+family("cdo")
+
