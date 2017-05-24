032b6bda2660db7df6a4105adaae9aba42f2036b
Adam Caprez <acaprez2@unl.edu>
2017-05-24 17:35:49 -0500

    Fix path.

--- Core/gmap/2017.05.08.lua
+++ Core/gmap/2017.05.08.lua
@@ -11,5 +11,5 @@ whatis("Keywords: Biology, Genomics, Sequencing, Alignment")
 whatis("Description: GMAP: A Genomic Mapping and Alignment Program for mRNA and EST Sequences, and GSNAP: Genomic Short-read Nucleotide Alignment Program.")
 whatis("URL: http://research-pub.gene.com/gmap")
 
-prepend_path{"PATH"		,"/util/opt/anaconda/4.3.14/envs/gmap-2017.05.08/bin",priority=100}
+prepend_path{"PATH"		,"/util/opt/anaconda/4.3/envs/gmap-2017.05.08/bin",priority=100}
 pushenv("CONDA_DEFAULT_ENV"	,"gmap-2017.05.08")
