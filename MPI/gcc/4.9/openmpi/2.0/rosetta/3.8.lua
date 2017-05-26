b2a6133b4c1f6ac7e1092444bdb213849c58e9a4
Qidong Jia <qidong@login.crane.hcc.unl.edu>
2017-05-26 13:44:38 -0500

    Rosetta added

---
+++ MPI/gcc/4.9/openmpi/2.0/rosetta/3.8.lua
@@ -0,0 +1,22 @@
+help (
+[[
+This module loads the Rosetta software suite, built with the GCC
+compiler/4.8 and openmpi/1.8.
+
+Documentation can be found online at https://www.rosettacommons.org/software.
+
+Version 3.8
+
+]])
+
+whatis("Name: rosetta")
+whatis("Version: 3.8")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Molecular dynamics")
+whatis("URL: https://www.rosettacommons.org/software")
+whatis("Description: The Rosetta software suite includes algorithms for computational modeling and analysis of protein structures. It has enabled notable scientific advances in computational biology, including de novo protein design, enzyme design, ligand docking, and structure prediction of biological macromolecules and macromolecular complexes. ")
+
+
+prepend_path("PATH","/util/opt/BCRF/rosetta/3.8/gcc/4.9/openmpi/2.0/bin")
+family("rosetta")
+
