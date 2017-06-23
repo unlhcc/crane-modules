1a858bbb0df93377145d25b10130e4332af21893
Adam Caprez <acaprez2@unl.edu>
2017-06-23 11:55:15 -0500

    Add module file for Singularity 2.3.

---
+++ Core/singularity/2.3.lua
@@ -0,0 +1,18 @@
+help (
+[[
+This module loads singularity built with GCC.
+This module makes available the singularity executable.
+
+Version 2.3
+]])
+
+whatis("Name: singularity")
+whatis("Version: 2.3")
+whatis("Category: system, container")
+whatis("Keywords:  System, Container")
+whatis("Description: Application containers enabling mobility of compute.")
+whatis("URL: http://singularity.lbl.gov")
+
+prepend_path("PATH"       ,"/util/opt/singularity/2.3/gcc/4.4/bin")
+setenv("SINGULARITY_IMAGES"  ,"/work/HCC/SINGULARITY/IMAGES")
+setenv("SINGULARITY_CACHEDIR", pathJoin(os.getenv("WORK"),".singularity"))
