ca096ae4a7285dbb042e034ce8e11e07463c6b66
Natasha Pavlovikj <npavlovikj2@unl.edu>
2017-06-26 13:21:02 -0500

    Add variable for gm_key and on_load message

--- Core/genemarks/4.3.lua
+++ Core/genemarks/4.3.lua
@@ -14,5 +14,10 @@ whatis("Keywords: Biology, Genomics")
 whatis("URL: http://exon.biology.gatech.edu/GeneMark")
 whatis("Description: GeneMarkS: a self-training method for prediction of gene starts in microbial genomes. Implications for finding sequence motifs in regulatory regions")
 
-os.execute("cp /util/opt/genemarks/4.3/gmsuite/gm_key $HOME/.gm_key")
+setenv("GM_KEY", "/util/opt/genemarks/4.3/gmsuite/gm_key")
 prepend_path("PATH", "/util/opt/genemarks/4.3/gmsuite")
+
+if mode() == "load" then
+  LmodMessage("If you are running genemarks for a first time, please copy the key to your HOME directory by typing:")
+  LmodMessage("cp $GM_KEY $HOME/.gm_key")
+end
