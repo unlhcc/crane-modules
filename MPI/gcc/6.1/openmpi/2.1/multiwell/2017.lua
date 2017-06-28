f16b2483bfb4d657c98704f816e5000e47cea814
Natasha Pavlovikj <npavlovikj2@unl.edu>
2017-06-28 00:49:24 -0500

    Add module file for MultiWell

---
+++ MPI/gcc/6.1/openmpi/2.1/multiwell/2017.lua
@@ -0,0 +1,14 @@
+help (
+[[
+This module loads MultiWell.
+Version 2017
+]])
+
+whatis("Name: MultiWell")
+whatis("Version: 2017")
+whatis("Category: chemistry, kinetic calculations, master equation")
+whatis("Keywords:  Stochastic Simulation, Monte Carlo, Master Equation")
+whatis("Description: MultiWell calculates time dependent concentrations, reaction yields, vibrational distributions, and rate constants as functions of temperature and pressure for multi-well, multi-channel unimolecular reactions systems that consist of stable species and multiple isomerization and/or dissociation reactions.")
+whatis("URL: http://clasp-research.engin.umich.edu/multiwell/index.php")
+
+prepend_path{"PATH","/util/opt/multiwell/2017/openmpi/2.1/gcc/6.1/bin"}
