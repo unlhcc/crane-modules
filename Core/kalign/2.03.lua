0b7dbc23731e8b47d437d99e743c192417b3ffb9
Natasha Pavlovikj <npavlovikj2@unl.edu>
2017-06-29 17:08:37 -0500

    Add module file for Kalign and update .xml file

---
+++ Core/kalign/2.03.lua
@@ -0,0 +1,18 @@
+help (
+[[
+This module loads Kalign.
+
+Version 2.03
+]])
+
+whatis("Name: Kalign")
+whatis("Version: 2.03")
+whatis("Category: computational biology, multi sequence alignment")
+whatis("Keywords:  Biology, Multi Sequence Alignment, DNA, RNA, Protein")
+whatis("Description: Kalign is a fast and accurate multiple sequence alignment algorithm designed to align large numbers of protein sequences.")
+whatis("URL: http://msa.sbc.su.se/cgi-bin/msa.cgi")
+
+pushenv("CONDA_DEFAULT_ENV","kalign-2.03")
+prepend_path{"PATH","/util/opt/anaconda/4.3/envs/kalign-2.03/bin",priority=100}
+
+family("anaconda")
