f2493271353f537ac130bb29d4a489f350230000
Natasha Pavlovikj <npavlovikj2@unl.edu>
2017-07-05 23:12:30 -0500

    Add module files for SNPTEST, IMPUTE2, QCTOOL, GTOOL, and update .xml file

---
+++ Core/qctool/1.4.lua
@@ -0,0 +1,14 @@
+help (
+[[
+This module loads QCTOOL.
+Version 1.4
+]])
+
+whatis("Name: QCTOOL")
+whatis("Version: 1.4")
+whatis("Category: computational biology, qwas, quality control")
+whatis("Keywords: Computational Biology, Genome-Wide Association Studies, SNP")
+whatis("Description: QCTOOL is a command-line utility program for basic quality control of gwas datasets.")
+whatis("URL: http://www.well.ox.ac.uk/~gav/qctool/#overview")
+
+prepend_path("PATH","/util/opt/qctool/1.4/bin")
