41705f7795afdb2090d9a488558d0ab77e6d09f7
Emelie Harstad <eharstad@unl.edu>
2017-08-22 15:27:56 -0500

    set MATLAB_PREFDIR to /tmp/matlab for r2014b module

--- Core/matlab/r2014b.lua
+++ Core/matlab/r2014b.lua
@@ -8,6 +8,7 @@ whatis("Name: MATLAB r2014b")
 whatis("Keywords: Application, Engineering, Math")
 
 local WORK = os.getenv("WORK")
-setenv("MATLAB_PREFDIR", WORK .. "/.matlab")
+--setenv("MATLAB_PREFDIR", WORK .. "/.matlab")
+setenv("MATLAB_PREFDIR", "/tmp/matlab")
 
 prepend_path("PATH","/util/opt/matlab/r2014b/bin")
