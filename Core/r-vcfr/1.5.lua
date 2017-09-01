b41bd02981838decebb8811e80d91a14dc70a32b
Natasha Pavlovikj <npavlovikj2@unl.edu>
2017-08-31 19:38:17 -0500

    Add module files for r-vcfr, r-seqminer, plink, plinkseq and update .xml file

---
+++ Core/r-vcfr/1.5.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads vcfR.
+Version 1.5.0
+
+]])
+
+whatis("Name: vcfR ")
+whatis("Version: 1.5.0")
+whatis("Category: biology, vcf file, visualization")
+whatis("Keywords: Biology, VCF file, Visualization")
+whatis("URL: https://github.com/knausb/vcfR")
+whatis("Description: A package to manipulate and visualize VCF data in R.")
+
+pushenv("CONDA_DEFAULT_ENV","r-vcfr-1.5.0")
+prepend_path{"PATH","/util/opt/anaconda/4.3/envs/r-vcfr-1.5.0/bin",priority=100}
