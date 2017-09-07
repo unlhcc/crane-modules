f75155dd562ee8d5a669271eaf3aab6bf823ba3f
Adam Caprez <acaprez2@unl.edu>
2017-09-07 14:53:28 -0500

    Add module file for DMTCP 2.5.

---
+++ Core/dmtcp/2.5.lua
@@ -0,0 +1,24 @@
+local help_message = [[
+DMTCP 2.5
+(Distributed MultiThreaded CheckPointing)
+This module loads DMTCP variables.
+The command directory is added to PATH.
+The library directory is added to LD_LIBRARY_PATH.
+The include directory is added to INCLUDE.
+The man     directory is added to MANPATH.
+]]
+
+help(help_message,"\n")
+
+whatis("Name: DMTCP")
+whatis("Version: 2.5")
+whatis("Category: Utility")
+whatis("Keywords: System, utility")
+whatis("URL: http://dmtcp.sourceforge.net")
+
+prepend_path("PATH",                "/util/opt/dmtcp/2.5/gcc/4.4/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/dmtcp/2.5/gcc/4.4/lib")
+prepend_path("LIBRARY_PATH",     "/util/opt/dmtcp/2.5/gcc/4.4/lib")
+prepend_path("MANPATH",             "/util/opt/dmtcp/2.5/gcc/4.4/share/man")
+prepend_path("INCLUDE",             "/util/opt/dmtcp/2.5/gcc/4.4/include")
+prepend_path("CPATH",             "/util/opt/dmtcp/2.5/gcc/4.4/include")
