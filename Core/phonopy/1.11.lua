c59f8a19712944922857fa95bb1b2ddb630d5aee
JingchaoZhang <zhang@unl.edu>
2017-09-08 11:24:40 -0500

    phonopy on Crane

---
+++ Core/phonopy/1.11.lua
@@ -0,0 +1,16 @@
+local help_message = [[
+Phonopy is an open source package for phonon calculations at harmonic and quasi-harmonic levels.
+]]
+
+help(help_message,"\n")
+
+whatis("Name: phonopy")
+whatis("Version: 1.11.12")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: https://atztogo.github.io/phonopy/index.html")
+
+prepend_path("PATH",                "/util/opt/phonopy/1.11/bin")
+prepend_path("PYTHONPATH",          "/util/opt/phonopy/1.11/lib/python2.7/site-packages")
+
+family("phonopy")
