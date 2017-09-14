206ed29007d799ed9f7624480f33616a5bca31fe
JingchaoZhang <zhang@unl.edu>
2017-09-14 16:21:01 -0500

    pindel on Crane

---
+++ Core/pindel/0.2.lua
@@ -0,0 +1,14 @@
+help (
+[[
+Pindel can detect breakpoints of large deletions, medium sized insertions, inversions, tandem duplications and other structural variants at single-based resolution from next-gen sequence data. It uses a pattern growth approach to identify the breakpoints of these variants from paired-end short reads.
+]])
+
+whatis("Name: pindel")
+whatis("Version: 0.2.5b8")
+whatis("Category: computational biology, genomics")
+whatis("Keywords:  Biology, Genomics, Alignment, Assembly")
+whatis("URL: https://github.com/genome/pindel")
+
+
+prepend_path("PATH"       ,"/util/opt/pindel/0.2/gcc/4.4/pindel")
+
