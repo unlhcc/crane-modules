b63cc20554872ad1e2f62a608107afb71af04615
JingchaoZhang <zhang@unl.edu>
2017-09-14 15:22:25 -0500

    siest on Crane

---
+++ MPI/gcc/4.9/openmpi/1.10/siesta/4.1.lua
@@ -0,0 +1,16 @@
+local help_message = [[
+SIESTA is popular computer software to perform ab initio density functional calculations of atomic clusters and lattices. The software is licensed free of charge to academic institutions and can be downloaded from http://www.uam.es/siesta.
+]]
+
+help(help_message,"\n")
+
+whatis("Name: SIESTA")
+whatis("Version: 4.1.3")
+whatis("Category: library, runtime support")
+whatis("Keywords: DFT, System, Library")
+whatis("URL: https://web.pa.msu.edu/people/tomanek/SIESTA-installation.html")
+
+load("intel-mkl/15")
+prepend_path("PATH",                "/util/opt/siesta/4.1/gcc/4.9/openmpi/1.10/bin/")
+
+family("siesta")
