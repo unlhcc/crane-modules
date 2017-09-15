70b46cb7c867f76c821002a8c6e634bb3bac9baf
Adam Caprez <acaprez2@unl.edu>
2017-09-15 13:22:28 -0500

    Change to bioconda install for pindel.

--- Core/pindel/0.2.lua
+++ Core/pindel/0.2.lua
@@ -10,5 +10,5 @@ whatis("Keywords:  Biology, Genomics, Alignment, Assembly")
 whatis("URL: https://github.com/genome/pindel")
 
 
-prepend_path("PATH"       ,"/util/opt/pindel/0.2/gcc/4.4/pindel")
-
+prepend_path("PATH"       ,"/util/opt/anaconda/4.3.14/envs/pindel-0.2.5b8/bin")
+setenv("CONDA_DEFAULT_ENV", "pindel-0.2.5b8")
