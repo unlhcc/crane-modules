c5b41d0e6e0c5a14cdd7098af5b1e0d88b1c1774
Adam Caprez <acaprez2@unl.edu>
2017-09-20 16:03:20 -0500

    Update root version.

--- Compiler/gcc/4.9/root/6.06.lua
+++ Compiler/gcc/4.9/root/6.06.lua
@@ -2,21 +2,21 @@ help(
 [[
 ROOT is a modular scientific software framework.
 
-Version 6.06.00
+Version 6.06.08
 ]]
 )
 
 whatis("Name: ROOT")
-whatis("Version: 6.06.00")
+whatis("Version: 6.06.08")
 whatis("Category: big data, visualisation")
 whatis("Keywords: Library")
 whatis("URL: https://root.cern.ch/")
 whatis("Description: It provides all the functionalities needed to deal with big data processing, statistical analysis, visualisation and storage.")
 
 
-prepend_path("PATH",                    "/util/opt/root/6.06.00/gcc/4.9/bin")
-prepend_path("LD_LIBRARY_PATH",         "/util/opt/root/6.06.00/gcc/4.9/lib")
-setenv("ROOTSYS",                       "/util/opt/root/6.06.00/gcc/4.9/")
+prepend_path("PATH",                    "/util/opt/root/6.06.08/gcc/4.9/bin")
+prepend_path("LD_LIBRARY_PATH",         "/util/opt/root/6.06.08/gcc/4.9/lib")
+setenv("ROOTSYS",                       "/util/opt/root/6.06.08/gcc/4.9/")
 
 
-prepend_path("LIBRARY_PATH",         "/util/opt/root/6.06.00/gcc/4.9/lib")
+prepend_path("LIBRARY_PATH",         "/util/opt/root/6.06.08/gcc/4.9/lib")
