fd89f52d177473065b22d6cf077ce151bd6ac259
JingchaoZhang <zhang@unl.edu>
2017-09-20 15:35:18 -0500

    MISIS on Crane

---
+++ Core/misis/1.0.lua
@@ -0,0 +1,23 @@
+help (
+[[
+MISIS displays sRNA reads as a histogram where the x-axis indicates positions of the 5p- or 3p-terminal nucleotide of sense and antisense sRNAs, respectively, along a given reference sequence or its selected region and the y-axis the number of reads starting (for sense sRNA) or ending (for antisense sRNA) at each position.
+]])
+
+whatis("Name: MISIS")
+whatis("Version: 1.0")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Alignment, Sequencing, Variant calling")
+whatis("URL: http://www.fasteris.com/apps/")
+
+pushenv("PATH"          , "/util/opt/misis/1.0")
+pushenv("CLASSPATH"     , "/util/opt/misis/1.0/lib/biojava-1.7.1.jar")
+pushenv("CLASSPATH"     , "/util/opt/misis/1.0/lib/biojava3-core-3.0.7.jar")
+pushenv("CLASSPATH"     , "/util/opt/misis/1.0/lib/jackson-annotations-2.1.1.jar")
+pushenv("CLASSPATH"     , "/util/opt/misis/1.0/lib/jackson-core-2.1.1.jar")
+pushenv("CLASSPATH"     , "/util/opt/misis/1.0/lib/jackson-databind-2.1.1.jar")
+pushenv("CLASSPATH"     , "/util/opt/misis/1.0/lib/jersey-client-1.18.1.jar")
+pushenv("CLASSPATH"     , "/util/opt/misis/1.0/lib/jersey-core-1.18.1.jar")
+pushenv("CLASSPATH"     , "/util/opt/misis/1.0/lib/PDFRenderer-0.9.0.jar")
+pushenv("CLASSPATH"     , "/util/opt/misis/1.0/lib/sam-1.77.jar")
+pushenv("CLASSPATH"     , "/util/opt/misis/1.0/lib/appMISIS.jar")
+
