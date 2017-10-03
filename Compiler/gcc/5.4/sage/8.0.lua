7a5d4244240cc9258ab128448ec248f6bbef2000
Adam Caprez <acaprez2@unl.edu>
2017-10-03 16:17:23 -0500

    Add module file for Sage 8.0.

---
+++ Compiler/gcc/5.4/sage/8.0.lua
@@ -0,0 +1,19 @@
+help(
+[[
+The sage module file defines the following environment variables:
+PATH for the location of the sage executables.
+
+Version 8.0
+]]
+)
+
+whatis("Name: Sage")
+whatis("Version: 8.0")
+whatis("Category: mathematics")
+whatis("Keywords: Mathematics")
+whatis("URL: http://www.sagemath.org/")
+whatis("Description: 'Sage is a free open-source mathematics software system licensed under the GPL. It builds on top of many existing open-source packages: NumPy, SciPy, matplotlib, Sympy, Maxima, GAP, FLINT, R and many more.' Mission: 'Creating a viable free open source alternative to Magma, Maple, Mathematica and Matlab.'")
+
+setenv("DOT_SAGE", pathJoin(os.getenv("WORK"),".sage"))
+prepend_path("PATH",              "/util/opt/sage/8.0/gcc/5.4")
+
