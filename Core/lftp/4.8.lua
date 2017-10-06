1234fa02b97c3d354c96733d282706445dd98216
Adam Caprez <acaprez2@unl.edu>
2017-10-06 12:53:25 -0500

    Add module file for lftp 4.8.

---
+++ Core/lftp/4.8.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads LFTP.
+Version 4.8.2
+]]
+)
+
+whatis("Name: lftp")
+whatis("Version: 4.8.2")
+whatis("Category: system, utility")
+whatis("Keywords: System, Utility")
+whatis("URL: http://lftp.yar.ru")
+whatis("Description: LFTP is a sophisticated file transfer program supporting a number of network protocols (ftp, http, sftp, fish, torrent).")
+
+pushenv("CONDA_DEFAULT_ENV","lftp-4.8.2")
+prepend_path("PATH","/util/opt/anaconda/4.3/envs/lftp-4.8.2/bin")
