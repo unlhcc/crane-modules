fcaef98c694a20415bfb80d588c4303249bf3872
JingchaoZhang <zhang@unl.edu>
2017-10-16 11:25:43 -0500

    twoBitToFa on Crane

---
+++ Core/twobittofa/1.0.lua
@@ -0,0 +1,15 @@
+help (
+[[
+It will be used to convert sequencing data from (.2bit) file format into
+(.fa) format, that is a necessary to start our analysis workflow.
+
+Version 1.0
+]])
+
+whatis("Name: twoBitToFa")
+whatis("Version: 1.0")
+whatis("Description: This is a bioinformatics, freely downloadable command-line software
+(linux.x86_64) provided by the UCSC")
+whatis("URL: https://hcc-rt.unl.edu/Ticket/Display.html?id=16468")
+
+prepend_path("PATH",			"/util/opt/twoBitToFa/1.0/gcc/4.4/bin")
