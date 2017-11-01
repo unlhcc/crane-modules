26d8cd7a456b8c781ccc595c30bfabd283bfe76b
Natasha Pavlovikj <npavlovikj2@unl.edu>
2017-11-01 12:30:23 -0500

    Rename directory

---
+++ Core/gdcclient/1.3.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads gdc-client.
+Version 1.3.0
+]]
+)
+
+whatis("Name: GDC Data Transfer Tool")
+whatis("Version: 1.3.0")
+whatis("Category: genomics, data transfer")
+whatis("Keywords: Biology, Data, Transfer")
+whatis("URL: https://gdc.cancer.gov/access-data/gdc-data-transfer-tool")
+whatis("Description: GDC provides a standard client-based mechanism in support of high performance data downloads and submission.")
+
+pushenv("CONDA_DEFAULT_ENV","gdc-client-1.3.0")
+prepend_path("PATH","/util/opt/anaconda/4.3/envs/gdc-client-1.3.0/bin")
