356a701374648d7411adeafb6c4115a5ccff088d
Adam Caprez <acaprez2@unl.edu>
2017-12-08 15:51:30 -0600

    Add module file for Singularity 2.4.

---
+++ Core/singularity/2.4.lua
@@ -0,0 +1,19 @@
+help (
+[[
+This module loads singularity built with GCC.
+This module makes available the singularity executable.
+
+Version 2.4
+]])
+
+whatis("Name: singularity")
+whatis("Version: 2.4")
+whatis("Category: system, container")
+whatis("Keywords:  System, Container")
+whatis("Description: Application containers enabling mobility of compute.")
+whatis("URL: http://singularity.lbl.gov")
+
+prepend_path("PATH"       ,"/util/opt/singularity/2.4/gcc/4.4/bin")
+setenv("SINGULARITY_IMAGES"  ,"/work/HCC/SINGULARITY/IMAGES")
+setenv("SINGULARITY_CACHEDIR", pathJoin(os.getenv("WORK"),".singularity"))
+setenv("REGISTRY", 	"hcc-docker-registry.unl.edu")
