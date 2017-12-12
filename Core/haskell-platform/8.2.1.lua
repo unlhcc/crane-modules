5332dce253995b8313779ad6d441af72558c8d21
Adam Caprez <acaprez2@unl.edu>
2017-12-12 17:40:02 -0600

    Add module file for haskell-platform 8.2.1.

---
+++ Core/haskell-platform/8.2.1.lua
@@ -0,0 +1,15 @@
+help (
+[[
+This module loads haskell-platform.
+
+Version 8.2.1.
+]])
+
+whatis("Name: haskell-platform")
+whatis("Version: 8.2.1")
+whatis("Category: Utilities, Language, Compiler")
+whatis("Keywords:  Utilities, Language, Compiler")
+whatis("Description: The Haskell Platform is a collection of software packages, tools and libraries that create a common platform for using and developing applications in Haskell..")
+whatis("URL: http://www.haskell.org/platform")
+
+prepend_path("PATH"       ,"/util/opt/haskell-platform/8.2.1/gcc/4.4/ghc-8.2.1-x86_64/bin")
