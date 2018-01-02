13b27eee614f5247455dedb617e6f0e8837a8a10
JingchaoZhang <zhang@unl.edu>
2018-01-02 17:35:18 -0600

    blat v34 on Crane

---
+++ Core/blat/34.lua
@@ -0,0 +1,18 @@
+help(
+[[
+The blat module file defines the following environment variables:
+PATH for the location of the blst distribution executables.
+
+Version 34
+]]
+)
+
+whatis("Name: BLAT")
+whatis("Version: 34")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Alignment")
+whatis("URL: http://genome.ucsc.edu/FAQ/FAQblat.html")
+whatis("Description: Blat is an alignment tool like BLAST, but it is structured differently. On DNA, Blat works by keeping an index of an entire genome in memory. Thus, the target database of BLAT is not a set of GenBank sequences, but instead an index derived from the assembly of the entire genome.")
+
+
+prepend_path("PATH",              "/util/opt/blat/34/bin")
