c8cd481013cd716a0bc91d09862038b1b96d275f
Adam Caprez <acaprez2@unl.edu>
2018-01-05 11:58:44 -0600

    Update MACS2 to 2.1.1.

--- Core/macs2/2.1.lua
+++ Core/macs2/2.1.lua
@@ -1,18 +1,17 @@
 help (
 [[
-This module loads MACS2, which depends on Python 2.7
+This module loads MACS2.
 PATH has been prepended to include the MACS2 program.
-Version 2.1.0
+Version 2.1.1
 ]])
 
 whatis("Name: macs2")
-whatis("Version: 2.1.0")
+whatis("Version: 2.1.1")
 whatis("Category: computational biology, sequencing")
 whatis("Keywords: Biology, Genomics, Sequencing")
 whatis("Description: MACS -- Model-based Analysis of ChIP-Seq")
 whatis("URL: https://github.com/taoliu/MACS")
 
-prepend_path{"PATH"		,"/util/opt/anaconda/2.2/envs/macs2-2.1.0/bin",priority=100}
-pushenv("CONDA_DEFAULT_ENV"	,"macs2-2.1.0")
-prereq("python/2.7")
+prepend_path{"PATH"		,"/util/opt/anaconda/4.3/envs/macs2-2.1.1.20160309/bin",priority=100}
+pushenv("CONDA_DEFAULT_ENV"	,"macs2-2.1.1.20160309")
 family("anaconda")
