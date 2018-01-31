9b34864f510e653b95af2a47d64b1ddfc36657d2
Adam Caprez <acaprez2@unl.edu>
2018-01-31 15:34:23 -0600

    Add module file for CUDA 9.0.

---
+++ Core/cuda/9.0.lua
@@ -0,0 +1,29 @@
+help(
+[[
+This module loads the NVIDIA CUDA Toolkit and updates the $PATH 
+$LD_LIBRARY_PATH, $INCLUDE, and $MANPATH environment 
+variables to access the toolkit binaries, libraries, include 
+files, and available man pages, respectively.
+
+Version 9.0
+]]
+)
+
+whatis("Name: NVIDIA CUDA Toolkit")
+whatis("Version: 9.0")
+whatis("Category: compiler, runtime support")
+whatis("Keywords: compiler, cuda")
+whatis("URL: http://www.nvidia.com/object/cuda_home_new.html")
+whatis("Description: NVIDIA CUDA Toolkit for Linux.")
+
+
+prepend_path("PATH",              "/util/opt/cuda/9.0/bin")
+prepend_path("LD_LIBRARY_PATH",   "/util/opt/cuda/9.0/lib64")
+prepend_path("LD_LIBRARY_PATH",   "/util/opt/cuda/9.0/lib")
+prepend_path("LD_LIBRARY_PATH",   "/usr/lib64/nvidia")
+prepend_path("LIBRARY_PATH",      "/util/opt/cuda/9.0/lib64")
+prepend_path("LIBRARY_PATH",      "/util/opt/cuda/9.0/lib")
+prepend_path("LIBRARY_PATH",      "/usr/lib64/nvidia")
+prepend_path("MANPATH",		  "/util/opt/cuda/9.0/doc/man")
+prepend_path("CPATH",		  "/util/opt/cuda/9.0/include")
+setenv("CUDA_HOME",		  "/util/opt/cuda/9.0")
