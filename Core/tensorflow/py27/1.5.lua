273719991c6b6fb6c130a2867e640b558fa6ed94
Adam Caprez <acaprez2@unl.edu>
2018-02-05 13:56:47 -0600

    Add module files for TensorFlow 1.5.

---
+++ Core/tensorflow/py27/1.5.lua
@@ -0,0 +1,15 @@
+help (
+[[
+This module loads tensorflow.
+Version 1.5.0
+]])
+
+whatis("Name: tensorflow")
+whatis("Version: 1.5.0")
+whatis("Category: machine learning, mathematics")
+whatis("Keywords: Machine Learning, Mathematics")
+whatis("Description: TensorFlow is an open source software library for numerical computation using data flow graphs.")
+whatis("URL: https://www.tensorflow.org")
+
+prepend_path{"PATH"		,"/util/opt/anaconda/4.3/envs/tensorflow-1.5.0-py27/bin",priority=100}
+pushenv("CONDA_DEFAULT_ENV"	,"tensorflow-1.5.0-py27")
