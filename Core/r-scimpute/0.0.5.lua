5ae7a2566eed3f8b27bbbafb3abcdd1c75435263
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-02-18 23:49:35 -0600

    Add module file for r-scImpute and update .xml file

---
+++ Core/r-scimpute/0.0.5.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads r-scImpute.
+Version 0.0.5
+
+]])
+
+whatis("Name: r-scImpute")
+whatis("Version: 0.0.5")
+whatis("Category: biology, scRNA-seq, normalization")
+whatis("Keywords: Biology, scRNA-sea, Gene Expression Analysis")
+whatis("URL: https://github.com/Vivianstats/scImpute")
+whatis("Description: scImpute is accurate and robust imputation of single-cell RNA sequencing data.")
+
+pushenv("CONDA_DEFAULT_ENV","r-scimpute-0.0.5")
+prepend_path{"PATH","/util/opt/anaconda/4.3/envs/r-scimpute-0.0.5/bin",priority=100}
