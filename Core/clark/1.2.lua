36c4c9918ad395215633e0b54f8e6dcd6c45d945
JingchaoZhang <zhang@unl.edu>
2018-02-26 14:15:25 -0600

    CLARK 1.2.5 on Crane

---
+++ Core/clark/1.2.lua
@@ -0,0 +1,13 @@
+help (
+[[
+ CLARK is a software tool for classifying any type of DNA/RNA sequences in any format (reads, contigs, scaffolds, etc.), generically called "objects", against a set of reference sequences (reads, contigs, assemblies, etc.), called "targets".
+Version 1.2
+]])
+
+whatis("Name: CLARK")
+whatis("Version: 1.2")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Quality Control, Utility, Sequencing")
+whatis("URL: http://clark.cs.ucr.edu/Overview/")
+
+prepend_path("PATH"       ,"/util/opt/clark/1.2/gcc/4.4/bin")
