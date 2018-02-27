6f44e9a4d63c1ec026f8ac9b905f36efb399e9cf
Tom Harvill <tharvill@login.crane.hcc.unl.edu>
2018-02-27 10:50:31 -0600

    lsdyna version 10.1.0

---
+++ Core/lsdyna/10.1.lua
@@ -0,0 +1,22 @@
+local help_message = [[
+LS-DYNA (Finite Element Analysis Software)
+  Provided for and used with permission of Prof. John Reid
+
+## This is a special dev version, only MPI version available ##
+
+mpp971_s    = Single precision parallel (OpenMPI)
+mpp971_d    = Double precision parallel (OpnenMPI)
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: LS-DYNA")
+whatis("Keywords: Application, Engineering")
+
+load("compiler/intel/16","openmpi/1.10")
+
+setenv("LSTC_LICENSE", "network")
+setenv("LSTC_LICENSE_SERVER", "10.138.31.201")
+
+prepend_path("PATH","/util/opt/lsdyna/r10.1/")
