f8c31e3a46b1142bd1e48a9616aaae8d481e90f2
Adam Caprez <acaprez2@unl.edu>
2018-02-28 16:52:03 -0600

    Add module file for PhyloSNP 1.0.

---
+++ Core/phylosnp/1.0.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads PhyloSNP
+PATH has been prepended to include the PhyloSNP program.
+Version 1.0
+]])
+
+whatis("Name: PhyloSNP")
+whatis("Version: 1.0	")
+whatis("Category: computational biology, sequencing")
+whatis("Keywords: Biology, Genomics, Sequencing")
+whatis("Description: PhyloSNP is designed to take SNP data files (.csv and .vcf) and generate phylogenetic trees from the provided data.")
+whatis("URL: https://hive.biochemistry.gwu.edu/dna.cgi?cmd=phylosnp")
+
+prepend_path{"PATH"		,"/util/opt/anaconda/4.3/envs/phylosnp-1.0/bin",priority=100}
+pushenv("CONDA_DEFAULT_ENV"	,"phylosnp-1.0")
