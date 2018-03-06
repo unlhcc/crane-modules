c940e8caee62a3b9f281c2e245e0d0d3e5114f00
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-03-06 11:42:16 -0600

    Add module for IMa2p

---
+++ MPI/gcc/6.1/openmpi/1.10/ima2p/1.0.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads IMa2p.
+Commit 58a0260.
+]]
+)
+
+whatis("Name: IMa2p")
+whatis("Version: 1.0")
+whatis("Category: Bayesian MCMC, population demography, IM model")
+whatis("Keywords: IMa2, Bayesian MCMC, IM")
+whatis("Description: IMa2p is a parallel implementation of IMa2, using OpenMPI-C++ - a Bayesian MCMC based method for inferring population demography under the IM (Isolation with Migration) model.")
+whatis("URL: https://github.com/arunsethuraman/ima2p")
+
+prepend_path("PATH", "/util/opt/ima2p/1.0/gcc/6.1/openmpi/1.10/bin/")
+
+family("ima2p")
