6cfce9ae8bb5631b0f39477d837de06ef8595f93
JingchaoZhang <zhang@unl.edu>
2018-03-07 10:30:13 -0600

    GcProfile on Crane

---
+++ Core/gcprofile/1.0.lua
@@ -0,0 +1,13 @@
+help (
+[[
+GC-Profile is a web-based tool for visualizing and analyzing the variation of GC content in genomic sequences.
+]])
+
+whatis("Name: gcprofile")
+whatis("Version: 1.0")
+whatis("Category: computational biology, genomics")
+whatis("Keywords:  Biology, Genomics, Sequencing, FastQ, Quality Control")
+whatis("Description: GC-Profile is a web-based tool for visualizing and analyzing the variation of GC content in genomic sequences")
+whatis("URL: http://tubic.tju.edu.cn/GC-Profile/")
+
+prepend_path("PATH",    "/util/opt/gcprofile/1.0/4.4/bin")
