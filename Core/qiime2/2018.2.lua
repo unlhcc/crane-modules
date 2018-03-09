6c49d2463f0a67f349e26a0ede5b68326d17d239
Adam Caprez <acaprez2@unl.edu>
2018-03-09 14:49:25 -0600

    Add Matplotlib backend variable.

--- Core/qiime2/2018.2.lua
+++ Core/qiime2/2018.2.lua
@@ -15,6 +15,7 @@ whatis("Description: QIIME 2 is a powerful, extensible, and decentralized microb
 pushenv("CONDA_DEFAULT_ENV","qiime2-2018.2")
 prepend_path{"PATH","/util/opt/BCRF/conda-envs/qiime2-2018.2/bin",priority=100}
 
+setenv("MPLBACKEND","Agg")
 -- The q2cli package (via click) creates a cache on first run.  By default, this is ~/.config
 -- If the first run is on a worker, this will fail.  Setting this var tells it to cache to
 -- $WORK/.config instead.
