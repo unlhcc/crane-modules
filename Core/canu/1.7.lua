65581a1efe5255903f765994b31e04b53380bd9a
Carrie Brown <cbrown58@unl.edu>
2018-04-09 14:42:43 -0500

    changed URL

--- Core/canu/1.7.lua
+++ Core/canu/1.7.lua
@@ -9,7 +9,7 @@ whatis("Version: 1.7")
 whatis("Category: computational biology, sequencing")
 whatis("Keywords: Biology, Genomics, Sequencing")
 whatis("Description: Canu is a fork of the Celera Assembler, designed for high-noise single-molecule sequencing (such as the PacBio RS II/Sequel or Oxford Nanopore MinION).")
-whatis("URL: https://software.broadinstitute.org/gatk/")
+whatis("URL: https://github.com/marbl/canu")
 
 prepend_path{"PATH", "/util/opt/anaconda/4.3/envs/canu-1.7/bin", priority=100}
 pushenv("CONDA_DEFAULT_ENV", "canu-1.7")
