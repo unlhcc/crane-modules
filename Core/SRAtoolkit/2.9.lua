81f8c770f7f6591e00edd4da8f89417ebf7a4284
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-05-17 02:38:43 +0000

    Add missing whatis

--- Core/SRAtoolkit/2.9.lua
+++ Core/SRAtoolkit/2.9.lua
@@ -8,6 +8,7 @@ whatis("Name: SRA Toolkit")
 whatis("Version: 2.9")
 whatis("Category: computational biology, genomics")
 whatis("Keywords: Biology, Genomics, High-throughput Sequencing")
-whatis("Description: SRA Toolkit")
+whatis("Description: The SRA Toolkit and SDK from NCBI is a collection of tools and libraries for using data in the INSDC Sequence Read Archives.")
+whatis("URL: https://github.com/ncbi/sra-tools")
 
 prepend_path("PATH", "/util/opt/sratools/2.9/bin")
