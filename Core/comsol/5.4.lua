b684258ae5c42ef82e8b58d244ad8ac8209e560b
Jingchao Zhang <jzhang30@unl.edu>
2019-04-09 16:24:46 +0000

    comsol v5.4 on Crane

---
+++ Core/comsol/5.4.lua
@@ -0,0 +1,18 @@
+help(
+[[
+This module loads COMSOL.
+
+Version 5.4
+]]
+)
+
+whatis("Name: COMSOL")
+whatis("Version: 5.4")
+whatis("Category: application, simulation")
+whatis("Keywords: application, simulation")
+whatis("URL: https://www.comsol.com")
+whatis("Description: COMSOL Multiphysics")
+
+
+prepend_path("PATH",              "/util/opt/comsol/5.4/comsol54/multiphysics/bin/")
+
