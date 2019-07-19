826dbca6fe1c18a618d48c53548e63f66e097678
Adam Caprez <acaprez2@unl.edu>
2019-07-19 19:59:33 +0000

    Add OMPI 3.1 built w/GCC 9.1.

---
+++ Compiler/gcc/9.1/openmpi/3.1.lua
@@ -0,0 +1,33 @@
+local help_message = [[
+OpenMPI Compilers 3.1
+
+This module loads the OpenMPI environment, built with the GCC
+compilers.  By loading this module, the following commands
+will be automatically available for compiling MPI applications:
+
+mpif77		(F77 source)
+mpif90		(F90 source)
+mpicc		(C source)
+mpiCC/mpicxx	(C++ source)
+]]
+
+help(help_message,"\n")
+
+whatis("Name: OpenMPI")
+whatis("Version: 3.1")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.open-mpi.org")
+
+prepend_path("PATH",                "/util/opt/openmpi/3.1/gcc/9.1/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/openmpi/3.1/gcc/9.1/lib")
+prepend_path("MANPATH",             "/util/opt/openmpi/3.1/gcc/9.1/share/man")
+prepend_path("INCLUDE",             "/util/opt/openmpi/3.1/gcc/9.1/include")
+
+local mroot = os.getenv("MODULEPATH_ROOT") or "/util/opt/modulefiles"
+local mdir = pathJoin(mroot,"MPI","gcc","9.1","openmpi","3.1")
+prepend_path("MODULEPATH",          mdir)
+
+prepend_path("CPATH",               "/util/opt/openmpi/3.1/gcc/9.1/include")
+prepend_path("LIBRARY_PATH",        "/util/opt/openmpi/3.1/gcc/9.1/lib")
+prepend_path("PKG_CONFIG_PATH",     "/util/opt/openmpi/3.1/gcc/9.1/lib/pkgconfig")
