dd3ba120051459c523ee3567422c8a6db7061768
Adam Caprez <acaprez2@unl.edu>
2019-08-02 19:38:11 +0000

    Add Java env vars to R 3.5 module.

--- Core/R/3.5.lua
+++ Core/R/3.5.lua
@@ -19,3 +19,13 @@ prepend_path("R_LIBS", "/util/opt/anaconda/deployed-conda-envs/packages/mro/envs
 prepend_path("LD_LIBRARY_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/mro/envs/mro-3.5.1/lib")
 prepend_path("LIBRARY_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/mro/envs/mro-3.5.1/lib")
 family("R")
+
+-- Java stuffs.  We set it here as env variables instead of in the R config to
+-- make it easier to override if a different version is needed.
+setenv("JAVA_LD_LIBRARY_PATH", "/util/opt/java/1.8/jre/lib/amd64/server")
+setenv("JAVA", "/util/opt/java/1.8/jre/bin/java")
+setenv("JAVA_HOME", "/util/opt/java/1.8")
+setenv("JAVAC", "/util/opt/java/1.8/bin/javac")
+setenv("JAVAH", "/util/opt/java/1.8/bin/javah")
+setenv("JAVA_CPPFLAGS", "-I/util/opt/java/1.8/include -I/util/opt/java/1.8/include/linux")
+setnev("JAVA_LIBS", "-L/util/opt/java/jdk1.8.0_162/jre/lib/amd64/server -ljvm -Wl,-rpath=/util/opt/java/1.8/jre/lib/amd64/server")
