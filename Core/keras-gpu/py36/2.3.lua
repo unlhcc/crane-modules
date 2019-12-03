c90133cc73b254f01ae6cb06752028ba57e25744
Natasha Pavlovikj <natasha.pavlovikj@huskers.unl.edu>
2019-12-03 06:44:34 +0000

    Add module file for keras-gpu 2.3.1

---
+++ Core/keras-gpu/py36/2.3.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads GPU-enabled Keras using the TensorFlow backend.
+Version 2.3.1
+]]
+)
+
+whatis("Name: Keras")
+whatis("Version: 2.3.1")
+whatis("Category: machine learning, mathematics")
+whatis("Keywords: Machine Learning, Mathematics")
+whatis("URL: https://keras.io")
+whatis("Description: Deep Learning Library for Theano and TensorFlow")
+
+pushenv("CONDA_DEFAULT_ENV", "keras-gpu-2.3.1-py36")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/keras-gpu/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/keras-gpu/envs/keras-gpu-2.3.1-py36/bin")
