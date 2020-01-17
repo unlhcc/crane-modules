5dcca11ca106a61e53ba6a8a976bce6505d478b5
Adam Caprez <acaprez2@unl.edu>
2020-01-17 22:21:20 +0000

    Add module file for mumax3/3.10.

---
+++ Core/mumax3/3.10.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Mumax3.
+Version 3.10.2ae87b9
+]]
+)
+whatis("Name: Mumax3")
+whatis("Version: 3.10.2ae87b9")
+whatis("Category: micromagnetics, cuda")
+whatis("Keywords: micromagnetics, cuda")
+whatis("URL: http://mumax.github.io")
+whatis("Description: GPU-accelerated micromagnetic simulator")
+
+pushenv("CONDA_DEFAULT_ENV", "mumax3-3.10.2ae87b9")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/mumax3/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/mumax3/envs/mumax3-3.10.2ae87b9/bin")
