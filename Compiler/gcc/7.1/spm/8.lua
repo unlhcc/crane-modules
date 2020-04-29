ba688fd15552715d06f35bba8b8a26254dff9d51
Natasha Pavlovikj <npavlovikj2@unl.edu>
2020-04-28 23:10:56 -0500

    Add module file for afq and update dependencies

--- Compiler/gcc/7.1/spm/8.lua
+++ Compiler/gcc/7.1/spm/8.lua
@@ -12,7 +12,7 @@ whatis("Keywords: matlab, fMRI, brain imaging")
 whatis("URL: https://www.fil.ion.ucl.ac.uk/spm/")
 whatis("Description: Statistical Parametric Mapping refers to the construction and assessment of spatially extended statistical processes used to test hypotheses about functional imaging data.")
 
-prepend_path("PATH", "/util/opt/spm/spm8-maint")
-prepend_path("MATLABPATH", "/util/opt/spm/spm8-maint")
+prereq("matlab/r2018b")
 
-prereq ("matlab/r2018b")
+prepend_path("PATH", "/util/opt/spm/spm8-maint")
+prepend_path("MATLABPATH", "/util/opt/spm/spm8-maint:/util/opt/spm/spm8-maint/toolbox/:/util/opt/spm/spm8-maint/toolbox/Beamforming:/util/opt/spm/spm8-maint/toolbox/DARTEL:/util/opt/spm/spm8-maint/toolbox/dcm_meeg:/util/opt/spm/spm8-maint/toolbox/DEM:/util/opt/spm/spm8-maint/toolbox/FieldMap:/util/opt/spm/spm8-maint/toolbox/HDW:/util/opt/spm/spm8-maint/toolbox/MEEGtools:/util/opt/spm/spm8-maint/toolbox/mixture:/util/opt/spm/spm8-maint/toolbox/Neural_Models:/util/opt/spm/spm8-maint/toolbox/Seg:/util/opt/spm/spm8-maint/toolbox/Shoot:/util/opt/spm/spm8-maint/toolbox/spectral:/util/opt/spm/spm8-maint/toolbox/SRender")
--- Core/vistasoft/0fa6c4a.lua
+++ Core/vistasoft/0fa6c4a.lua
@@ -12,7 +12,7 @@ whatis("Keywords: matlab, MRI, neuroimaging")
 whatis("URL: http://vistalab.stanford.edu/")
 whatis("Description: VISTASOFT contains Matlab code to perform a variety of analysis on MRI data, including functional MRI and diffusion MRI.")
 
-prepend_path("PATH", "/util/opt/vistasoft/0fa6c4a")
-prepend_path("MATLABPATH", "/util/opt/vistasoft/0fa6c4a")
+prereq("spm")
 
-prereq ("matlab", "spm")
+prepend_path("PATH", "/util/opt/vistasoft/0fa6c4a")
+prepend_path("MATLABPATH", "/util/opt/vistasoft/0fa6c4a/:/util/opt/vistasoft/0fa6c4a/external:/util/opt/vistasoft/0fa6c4a/fileFilters:/util/opt/vistasoft/0fa6c4a/local:/util/opt/vistasoft/0fa6c4a/mrAlign:/util/opt/vistasoft/0fa6c4a/mrAnatomy:/util/opt/vistasoft/0fa6c4a/mrBOLD:/util/opt/vistasoft/0fa6c4a/mrDiffusion:/util/opt/vistasoft/0fa6c4a/mrMesh:/util/opt/vistasoft/0fa6c4a/mrQuant:/util/opt/vistasoft/0fa6c4a/mrScripts:/util/opt/vistasoft/0fa6c4a/mrTest:/util/opt/vistasoft/0fa6c4a/mrViewer:/util/opt/vistasoft/0fa6c4a/qa:/util/opt/vistasoft/0fa6c4a/rdt:/util/opt/vistasoft/0fa6c4a/utilities:/util/opt/vistasoft/0fa6c4a/tutorials")
