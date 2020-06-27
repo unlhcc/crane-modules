d39a2c78320935d00bd1299e14ee454b467ff29d
Natasha Pavlovikj <natasha.pavlovikj@huskers.unl.edu>
2020-06-27 21:13:14 +0000

    Add module file for OpenFoam 7

--- MPI/gcc/4.9/openmpi/1.10/openfoam/6.0.lua
+++ MPI/gcc/4.9/openmpi/1.10/openfoam/6.0.lua
@@ -1,7 +1,6 @@
 local help_message = [[
 OpenFOAM 6
 OpenFOAM is professionally released every six months to include customer sponsored developments and contributions from the community. It is independently tested by ESI-OpenCFD's Application Specialists, Development Partners and selected customers, and supported by ESI's worldwide infrastructure, values and commitment.
-
 ]]
 
 help(help_message,"\n")
@@ -12,6 +11,14 @@ whatis("Category: library, runtime support")
 whatis("Keywords: System, Library")
 whatis("URL: https://www.openfoam.com/")
 
-prepend_path("PATH",        "/util/opt/openfoam/6/gcc/4.9/openmpi/1.10/OpenFOAM-6/bin")
-setenv("OPENFOAM_BASHRC",   "/util/opt/openfoam/6/gcc/4.9/openmpi/1.10/OpenFOAM-6/etc/bashrc")
+prepend_path("PATH", "/util/opt/openfoam/6/gcc/4.9/openmpi/1.10/OpenFOAM-6/bin/")
+setenv("OPENFOAM_BASHRC", "/util/opt/openfoam/6/gcc/4.9/openmpi/1.10/OpenFOAM-6/etc/bashrc")
+execute {cmd="source /util/opt/openfoam/6/gcc/4.9/openmpi/1.10/OpenFOAM-6/etc/bashrc", modeA={"load"}}
 
+if (mode() == "unload") then
+  remove_path("PATH", "/util/opt/openfoam/6/gcc/4.9/openmpi/1.10/ThirdParty-6/platforms/linux64Gcc/gperftools-svn/bin")
+  remove_path("PATH", "/util/opt/openfoam/6/gcc/4.9/openmpi/1.10/site/6/platforms/linux64GccDPInt32Opt/bin")
+  remove_path("PATH", "/util/opt/openfoam/6/gcc/4.9/openmpi/1.10/OpenFOAM-6/platforms/linux64GccDPInt32Opt/bin")
+  remove_path("PATH", "/util/opt/openfoam/6/gcc/4.9/openmpi/1.10/OpenFOAM-6/bin")
+  remove_path("PATH", "/util/opt/openfoam/6/gcc/4.9./openmpi/1.10/OpenFOAM-6/wmake")
+end
