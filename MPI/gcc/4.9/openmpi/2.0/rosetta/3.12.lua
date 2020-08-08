c979fc05a28bbab1ddfeeb0a45b31381fe0120d2
Jean-Jack N00b Riethoven <jriethoven2@unl.edu>
2020-08-08 14:10:12 -0500

    Fixed library paths for Rosetta tools.

--- MPI/gcc/4.9/openmpi/2.0/rosetta/3.12.lua
+++ MPI/gcc/4.9/openmpi/2.0/rosetta/3.12.lua
@@ -39,10 +39,26 @@ whatis("URL: https://www.rosettacommons.org/software")
 whatis("Description: The Rosetta software suite includes algorithms for computational modeling and analysis of protein structures. It has enabled notable scientific advances in computational biology, including de novo protein design, enzyme design, ligand docking, and structure prediction of biological macromolecules and macromolecular complexes. ")
 
 load("biopython/py27/1.76")
-setenv("ROSETTA3", "/util/opt/BCRF/rosetta/3.12/gcc/4.9/openmpi/2.0/")
-setenv("ROSETTA3_DB", "/util/opt/BCRF/rosetta/3.12/gcc/4.9/openmpi/2.0/database")
-setenv("ROSETTA_TOOLS", "/util/opt/BCRF/rosetta/3.12/gcc/4.9/openmpi/2.0/tools/")
+setenv("ROSETTA3", "/util/opt/BCRF/rosetta/3.12/gcc/4.9/openmpi/2.0/main/")
+setenv("ROSETTA3_DB", "/work/HCC/BCRF/app_specific/rosetta/3.12/database/")
+setenv("ROSETTA_TOOLS", "/util/opt/BCRF/rosetta/3.12/gcc/4.9/openmpi/2.0/main/source/tools/")
 
-prepend_path("PATH","/util/opt/BCRF/rosetta/3.12/gcc/4.9/openmpi/2.0/bin")
+-- All kinds of scripts and tools are self-contained with their own directory structure dependencies hence it
+-- is not easy to copy them all into a single folder. On top of that, there are scripts with the same name
+-- and different code. So, append paths to the most-often used internal tools or apps seems to be the 
+-- most straight forward.
+
+prepend_path("PATH","/util/opt/BCRF/rosetta/3.12/gcc/4.9/openmpi/2.0/main/source/bin")
+prepend_path("PATH","/util/opt/BCRF/rosetta/3.12/gcc/4.9/openmpi/2.0/main/tools/")
+prepend_path("PATH","/util/opt/BCRF/rosetta/3.12/gcc/4.9/openmpi/2.0/main/source/scripts/python/public/")
+prepend_path("PATH","/util/opt/BCRF/rosetta/3.12/gcc/4.9/openmpi/2.0/main/tools/protein_tools/scripts/")
+
+-- there are some demo programs that are not compiled from source, and they are referring to libraries
+-- in the wrong locations. Add the library path so these programs can be run.
+-- (mostly: some refer to libraries that do not exist in the current version anymore. For those binaries
+-- user has to manually change to <name>.mpi.linuxgccrelease
+prepend_path("LD_LIBRARY_PATH","/util/opt/BCRF/rosetta/3.12/gcc/4.9/openmpi/2.0/main/source/build/src/release/linux/2.6/64/x86/gcc/4.9/mpi")
+
+
 
 family("rosetta")
