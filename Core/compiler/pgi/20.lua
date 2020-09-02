c39a24890c7131f5d2f7148749883aad49af1f49
Adam Caprez <acaprez2@unl.edu>
2020-09-02 22:30:17 +0000

    Add module files for PGI 19 and 20.

---
+++ Core/compiler/pgi/20.lua
@@ -0,0 +1,42 @@
+local help_message = [[
+PGI Compilers 2020
+
+This module loads PGI Compiler variables.
+The command directory is added to PATH.
+The library directory is added to LD_LIBRARY_PATH.
+The include directory is added to INCLUDE.
+The man     directory is added to MANPATH.
+]]
+
+help(help_message,"\n")
+
+whatis("Name: PGI Compilers")
+whatis("Version: 20")
+whatis("Category: compiler")
+whatis("Keywords: System, compiler")
+whatis("URL: http://www.pgroup.com")
+
+prepend_path("PATH",                "/util/comp/pgi/2020/linux86-64/2020/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/comp/pgi/2020/linux86-64/2020/lib")
+prepend_path("LIBRARY_PATH",     "/util/comp/pgi/2020/linux86-64/2020/lib")
+prepend_path("MANPATH",             "/util/comp/pgi/2020/linux86-64/2020/man")
+prepend_path("CPATH",             "/util/comp/pgi/2020/linux86-64/2020/include")
+
+local mroot = os.getenv("MODULEPATH_ROOT") or "/util/opt/modulefiles"
+local mdir = pathJoin(mroot,"Compiler","pgi","20")
+prepend_path("MODULEPATH",          mdir)
+
+setenv("CFLAGS",        "-tp=sandybridge")
+setenv("FFLAGS",        "-tp=sandybridge")
+setenv("CXXFLAGS",        "-tp=sandybridge")
+setenv("F90FLAGS",        "-tp=sandybridge")
+setenv("FCFLAGS",        "-tp=sandybridge")
+
+setenv("CC",	"pgcc")
+setenv("FC",	"pgf90")
+setenv("F90",	"pgf90")
+setenv("F77",	"pgf77")
+setenv("CPP",	"pgcc -E")
+setenv("CXX",	"pgc++")
+
+family("compiler")
