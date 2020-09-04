40f16da007a873a1a60f24cd6b6acfbb7c337d69
Adam Caprez <acaprez2@unl.edu>
2020-09-04 06:35:39 +0000

    Wrf deps 2

---
+++ Compiler/pgi/20/hdf4/4.2.lua
@@ -0,0 +1,25 @@
+local help_message = [[
+hdf4 4.2.15
+
+This module loads the hdf4, built with the PGI 
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: hdf4")
+whatis("Version: 4.2.15")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.hdfgroup.org/release4/obtain.html#obtain")
+
+prepend_path("PATH",                "/util/opt/hdf4/4.2.15/pgi/20/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/hdf4/4.2.15/pgi/20/lib")
+prepend_path("MANPATH",             "/util/opt/hdf4/4.2.15/pgi/20/share/man")
+prepend_path("INCLUDE",             "/util/opt/hdf4/4.2.15/pgi/20/include")
+
+family("hdf4")
+
+prepend_path("CPATH",             "/util/opt/hdf4/4.2.15/pgi/20/include")
+prepend_path("LIBRARY_PATH",     "/util/opt/hdf4/4.2.15/pgi/20/lib")
---
+++ Compiler/pgi/20/netcdf/4.7.lua
@@ -0,0 +1,36 @@
+local help_message = [[
+netCDF 4.7
+
+This module loads netCDF C, Fortran, and CXX4, built with the PGI
+compilers.
+
+The package versions are:
+NetCDF C: 4.7
+NetCDF Fortran: 4.4
+NetCDF CXX4: 4.3
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: netCDF")
+whatis("Version: 4.7")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: https://www.unidata.ucar.edu/software/netcdf/docs/index.html")
+
+prepend_path("NETCDF",              "/util/opt/netcdf/4.7/pgi/20")
+prepend_path("NCDIR",              "/util/opt/netcdf/4.7/pgi/20")
+prepend_path("PATH",                "/util/opt/netcdf/4.7/pgi/20/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/netcdf/4.7/pgi/20/lib")
+prepend_path("MANPATH",             "/util/opt/netcdf/4.7/pgi/20/share/man")
+prepend_path("INCLUDE",             "/util/opt/netcdf/4.7/pgi/20/include")
+
+family("netcdf")
+family("netcdfc")
+family("netcdffortran")
+family("netcdfcxx4")
+
+prepend_path("CPATH",             "/util/opt/netcdf/4.7/pgi/20/include")
+prepend_path("LIBRARY_PATH",     "/util/opt/netcdf/4.7/pgi/20/lib")
+prepend_path("PKG_CONFIG_PATH",        "/util/opt/netcdf/4.7/pgi/20/lib/pkgconfig")
