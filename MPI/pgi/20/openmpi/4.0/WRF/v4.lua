3cd5a82da1bd9b8fef6f7695819bb93a2c42c47e
Adam Caprez <acaprez2@unl.edu>
2020-09-24 14:50:05 +0000

    Add perl and binutils to WRFv4 module.

--- MPI/pgi/20/openmpi/4.0/WRF/v4.lua
+++ MPI/pgi/20/openmpi/4.0/WRF/v4.lua
@@ -18,7 +18,7 @@ whatis("URL: http://www.wrf-model.org/index.php")
 -- -- the PATH variable ensures that the netcdf 'ncdump' is first in
 -- -- the path instead of the hdf4 one.
 -- -- See RT #7945.
-load("NCL/6.6","NCO/4.9","udunits2/2.2","hdf5/1.12","hdf4/4.2","netcdf/4.7","jasper/2.0","cdo/1.9","geos/3.8","proj/7.1","libgeotiff/1.6","wgrib/1.8","grib-api/1.21","ncview/2.1")
+load("NCL/6.6","NCO/4.9","udunits2/2.2","hdf5/1.12","hdf4/4.2","netcdf/4.7","jasper/2.0","cdo/1.9","geos/3.8","proj/7.1","libgeotiff/1.6","wgrib/1.8","grib-api/1.21","ncview/2.1","perl/5.26","binutils/2.30")
 setenv("WRFIO_NCD_LARGE_FILE_SUPPORT", "1")
 
 family("WRF")
