c34dc759a5bb1e8892d1d5c9e31067b4661d22cc
Adam Caprez <acaprez2@unl.edu>
2020-10-14 00:09:01 +0000

    Ok, just load them all.

--- MPI/pgi/20/openmpi/4.0/WRF/v4.lua
+++ MPI/pgi/20/openmpi/4.0/WRF/v4.lua
@@ -18,7 +18,7 @@ whatis("URL: http://www.wrf-model.org/index.php")
 -- -- the PATH variable ensures that the netcdf 'ncdump' is first in
 -- -- the path instead of the hdf4 one.
 -- -- See RT #7945.
-load("NCL/6.6","NCO/4.9","udunits2/2.2","hdf5/1.12","hdf4/4.2","netcdf/4.7","jasper/2.0","cdo/1.9","geos/3.8","proj/7.1","libgeotiff/1.6","wgrib/1.8","grib-api/1.21","ncview/2.1","perl/5.26","binutils/2.30","expat/2.2")
+load("NCL/6.6","NCO/4.9","udunits2/2.2","hdf5/1.12","hdf4/4.2","netcdf/4.7","jasper/2.0","cdo/1.9","geos/3.8","proj/7.1","libgeotiff/1.6","wgrib/1.8","grib-api/1.21","ncview/2.1","perl/5.26","binutils/2.30","expat/2.2","bzip2/1.0","libpng/1.6","cairo/1.16","freetype/2.10")
 setenv("WRFIO_NCD_LARGE_FILE_SUPPORT", "1")
 setenv("LIBXEXT","/util/opt/libxext/1.3")
 prepend_path("LD_LIBRARY_PATH", "/util/opt/libxext/1.3/lib")
