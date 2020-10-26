cf6391d2099af0a32ed3fcde77fa99930723f7ef
Tom Harvill <tharvill@login.crane.hcc.unl.edu>
2020-10-26 10:07:24 -0500

    lsdyna r11 module

---
+++ Core/lsdyna/11.lua
@@ -0,0 +1,55 @@
+local help_message = [[
+LS-DYNA (Finite Element Analysis Software)
+
+## This is a special dev version, only MPI version available ##
+
+mpp971_s    = Single precision parallel (OpenMPI)
+mpp971_d    = Double precision parallel (OpnenMPI)
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: LS-DYNA")
+whatis("Keywords: Application, Engineering")
+
+load("compiler/intel/18","intel-mpi")
+
+--
+-- Logic to allow only certain HCC groups to use LS-DYNA
+--
+myUser = os.getenv("USER")
+tmpfile = "/tmp/"..myUser
+os.execute("/usr/bin/groups > "..tmpfile)
+local file,err = io.open(tmpfile, "r")
+if file==nil then
+    io.stderr:write("Couldn't open file: "..err)
+else
+  for line in file:lines() do
+    for word in line:gmatch("%w+") do
+      --
+      -- CHECK GROUP MEMBERSHIP
+      --
+      if word == "reid" or word == "mech950" then
+        setenv("LSTC_LICENSE", "network")
+        setenv("LSTC_LICENSE_SERVER", "10.138.31.201")
+      elseif word == "wittichlab" or word == "swanson" or word == "dlinzell" or word == "skimlab" or word == "cematerials" or word == "simlab" then
+        setenv("LSTC_LICENSE", "network")
+        setenv("LSTC_LICENSE_SERVER", "10.138.31.207")
+      elseif word == "bobaru" or word == "mnegahban" then --UNL Mechanical Engineering
+        setenv("LSTC_LICENSE", "network")
+        setenv("LSTC_LICENSE_SERVER", "10.138.31.208")
+      elseif word == "cbbl" then
+        setenv("LSTC_LICENSE", "network")
+        setenv("LSTC_LICENSE_SERVER", "10.138.31.209")
+      end
+      -- io.stderr:write(word.."\n")
+    end
+  end
+  os.execute("/bin/rm "..tmpfile)
+end
+
+-- setenv("LSTC_LICENSE", "network")
+-- setenv("LSTC_LICENSE_SERVER", "10.138.31.201")
+
+prepend_path("PATH","/util/opt/lsdyna/r11/")
