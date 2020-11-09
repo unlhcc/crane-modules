85b9d55bb76775023a95fc8c5edf71cbc04c84c0
Adam Caprez <acaprez2@unl.edu>
2020-11-09 23:16:01 +0000

    Change to using env with tf-gpu conda package.

--- Core/tensorflow-gpu/py37/2.3.lua
+++ Core/tensorflow-gpu/py37/2.3.lua
@@ -11,8 +11,8 @@ whatis("Keywords: Machine Learning, Mathematics")
 whatis("Description: TensorFlow is an open source software library for numerical computation using data flow graphs.")
 whatis("URL: https://www.tensorflow.org")
 
-pushenv("CONDA_DEFAULT_ENV", "tensorflow-gpu-2.3.0-py37")
+pushenv("CONDA_DEFAULT_ENV", "tensorflow-gpu-2.3.0-py37.2")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tensorflow-gpu/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tensorflow-gpu/envs/tensorflow-gpu-2.3.0-py37/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tensorflow-gpu/envs/tensorflow-gpu-2.3.0-py37.2/bin")
 
 family("python")
--- Core/tensorflow-gpu/py38/2.3.lua
+++ Core/tensorflow-gpu/py38/2.3.lua
@@ -11,8 +11,8 @@ whatis("Keywords: Machine Learning, Mathematics")
 whatis("Description: TensorFlow is an open source software library for numerical computation using data flow graphs.")
 whatis("URL: https://www.tensorflow.org")
 
-pushenv("CONDA_DEFAULT_ENV", "tensorflow-gpu-2.3.0-py38")
+pushenv("CONDA_DEFAULT_ENV", "tensorflow-gpu-2.3.0-py38.2")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tensorflow-gpu/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tensorflow-gpu/envs/tensorflow-gpu-2.3.0-py38/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tensorflow-gpu/envs/tensorflow-gpu-2.3.0-py38.2/bin")
 
 family("python")
